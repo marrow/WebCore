cccaebd4c45747ff48f58681aa544fa5b6f4e4c3
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-06-08 22:58:26 -0700

    Prefixed configuration options with 'web.' to prevent confusion and add stub DB middleware.

--- web/core/application.py
+++ web/core/application.py
@@ -31,39 +31,61 @@ class Application(object):
     def factory(cls, gconfig=dict(), root=None, **config):
         """Build a full-stack framework around this WSGI application."""
         
-        from paste.deploy.converters import asbool, asint
+        log = __import__('logging').getLogger(__name__ + ':factory')
+        log.info("Preparing YAPWF WSGI middleware stack.")
+        
+        from web.core import Controller
         from web.utils.object import get_dotted_object
+        from paste.deploy.converters import asbool, asint
+        
+        if 'web.root' in config: root = config.get('web.root')
         
         # Find, build, and configure our basic Application instance.
         if isinstance(root, basestring):
+            log.debug("Loading root controller from '%s'.", root)
             root = get_dotted_object(root)
+        elif not isinstance(root, Controller):
+            raise ValueError("The root controller must be defined using package dot-colon-notation or direct reference.")
         app = cls(root(), **config)
         
         # Automatically use Buffet templating engines unless explicitly forbidden.
-        if asbool(config.get('buffet', True)):
+        if asbool(config.get('web.buffet', True)):
+            log.debug("Loading Buffet template engine middleware.")
             from web.core.middleware import TemplatingMiddleware
             app = TemplatingMiddleware(app, config)
         
         # Automatically use ToscaWidgets unless explicitly forbidden (or simply not found).
-        if asbool(config.get('widgets', True)):
+        if asbool(config.get('web.widgets', True)):
             try:
                 from tw.api import make_middleware as ToscaWidgetsMiddleware
                 
+                log.debug("Loading ToscaWidgets middleware.")
                 app = ToscaWidgetsMiddleware(app, config)
             
             except ImportError:
-                log.warn("ToscaWidgets not installed, widget framework disabled.  You can remove this warning by specifying widgets=False in your config.")
+                log.warn("ToscaWidgets not installed, widget framework disabled.  You can remove this warning by explicitly defining widgets=False in your config.")
+        
+        # Determine if a database engine has been requested, and load the appropriate middleware.
+        # Todo: YAPWF entry point for database engine middleware.
+        if config.get('db.engine', None) == 'sqlalchemy':
+            try:
+                log.debug("Loading SQLAlchemy session middleware.")
+            
+            except ImportError:
+                log.error("Unable to load requested database engine middleware, %s.", config.get('db.engine', None))
+        
         
         from paste.config import ConfigMiddleware
         app = ConfigMiddleware(app, config)
         
         # Automatically use beaker-supplied services unless explicitly forbidden (or simply not found).
-        if asbool(config.get('beaker', True)):
+        if asbool(config.get('web.beaker', True)):
             try:
                 from beaker.middleware import SessionMiddleware, CacheMiddleware
                 
                 beakerconfig = dict([(i[7:], j) for i, j in config.iteritems() if i.startswith('beaker.')])
                 
+                log.debug("Loading Beaker session and cache middleware.")
                 app = SessionMiddleware(app, beakerconfig)
                 app = CacheMiddleware(app, beakerconfig)
                 
@@ -74,31 +96,36 @@ class Application(object):
         
         try:
             if asbool(config.get('debug', False)):
+                log.debug("Debugging enabled; exceptions raised will display an interactive traceback.")
+                
                 from weberror.evalexception import EvalException
                 app = EvalException(app, gconfig)
             
             else:
+                log.debug("Debugging disabled; exceptions raised will display a 500 error.")
+                
                 from weberror.errormiddleware import ErrorMiddleware
                 app = ErrorMiddleware(app, gconfig)
         
         except ImportError:
-            log.warn("WebError not installed, pretty error messages disabled.")
+            log.warn("WebError not installed, interactive exception handling and messaging disabled.")
         
         
         from paste.registry import RegistryManager
         app = RegistryManager(app)
         
         
-        if asbool(config.get('profile', False)):
+        if asbool(config.get('web.profile', False)):
+            log.debug("Enabling profiling support.")
             from paste.debug.profile import ProfileMiddleware
-            app = ProfileMiddleware(app, log_filename=config.get('profile.file', 'profile.log.tmp'), limit=asint(config.get('profile.limit', 40)))
+            app = ProfileMiddleware(app, log_filename=config.get('web.profile.file', 'profile.log.tmp'), limit=asint(config.get('web.profile.limit', 40)))
         
         # Enabled explicitly or while debugging so you can use Paste's HTTP server.
-        if asbool(config.get('static', False)) or asbool(config.get('debug', False)):
+        if asbool(config.get('web.static', False)) or asbool(config.get('debug', False)):
             from paste.cascade import Cascade
             from paste.fileapp import DirectoryApp
             
-            path = config.get('static.path', None)
+            path = config.get('web.static.path', None)
             
             if path is None:
                 # Attempt to discover the path automatically.
@@ -110,12 +137,18 @@ class Application(object):
             if not os.path.isdir(path):
                 log.warn("Unable to find folder to serve static content from.  Please specify static.path in your config.")
             
+            log.debug("Serving static files from '%s'.", path)
+            
             app = Cascade([DirectoryApp(path), app], catch=[401, 403, 404])
         
         # Enable compression if requested.
-        if asbool(config.get('compress', False)):
+        if asbool(config.get('web.compress', False)):
+            log.debug("Enabling HTTP compression.")
+            
             from paste.gzipper import middleware as GzipMiddleware
-            app = GzipMiddleware(app, compress_level=asint(config.get('compress.level', 6)))
+            app = GzipMiddleware(app, compress_level=asint(config.get('web.compress.level', 6)))
+        
+        log.info("YAPWF WSGI middleware stack ready.")
         
         return app
     
