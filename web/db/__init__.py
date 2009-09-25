0f643359c716c6dec193acc89b039716b7000201
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-09-25 02:06:51 -0700

    Namespaced the db package.

--- web/db/__init__.py
+++ web/db/__init__.py
@@ -1,3 +1,4 @@
-# encoding: utf-8
-
-__all__ = ['api', 'sqlalchemy']
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    __import__('pkgutil').extend_path(__path__, __name__)
