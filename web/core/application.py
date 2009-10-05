aa11a534995f8657e458c63004f698c704e49843
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-10-05 03:32:04 -0700

    Abstracted out the middleware stack into a complete dependency graph generator.  Possibly overkill, but much easier to extend at any point.

--- web/core/application.py
+++ web/core/application.py
@@ -7,6 +7,9 @@ import sys, os, pkg_resources, types
 
 import paste
 from webob import Request, Response
+
+import web
+from web.core import middleware
 from web.core.dialects import Dialect
 
 
@@ -26,19 +29,53 @@ class Application(object):
     environment.
     """
     
-    def __init__(self, root, **kw):
+    def __init__(self, root=None, config=dict()):
         self.root = root
-        self.config = kw
+        self.config = config
         
         from web.core import config
-        config.update(kw)
-        
-        # Allow loading of the root instance to be delayed or programatically adjusted.
-        if callable(self.root) and not isinstance(self.root, Dialect):
-            self.root = self.root()
+        config.update(config)
         
         if not isinstance(self.root, Dialect):
             raise TypeError('Root controller must be dialect subclass.')
+    
+    @classmethod
+    def middleware(cls):
+        """Generate a sorted dependancy graph of middleware.
+        
+        Might not be the most efficient implementation, but it works and is only run on startup.
+        
+        The @web.core.middleware.middleware decorator can be used to register middleware.
+        
+        This allows you to inject middleware at any point in the stack and even override default
+        layers (while still maintaining dependancy graph resolution).
+        """
+        
+        queue = [i for i in middleware.registry if i.after is None]
+        satisfied = []
+        
+        while queue:
+            obj = queue.pop(0)
+            satisfied.append(obj.name)
+            
+            yield obj
+            
+            for obj in middleware.registry:
+                if obj.name in satisfied or obj in queue:
+                    continue
+                
+                if isinstance(obj.after, list):
+                    deps = [(i in satisfied) for i in obj.after]
+                    if not all(deps):
+                        continue
+                
+                elif obj.after is "*" and queue:
+                    continue
+                
+                elif obj.after not in satisfied:
+                    continue
+                
+                queue.append(obj)
     
     @classmethod
     def factory(cls, gconfig=dict(), root=None, **config):
@@ -59,145 +96,18 @@ class Application(object):
             log.debug("Loading root controller from '%s'.", root)
             root = get_dotted_object(root)
         
-        if not issubclass(root, Dialect):
-            raise ValueError("The root controller must be defined using package dot-colon-notation or direct reference.")
+        if not issubclass(root, Dialect) and not callable(root):
+            raise ValueError("The root controller must be defined using package dot-colon-notation or direct reference and must be either a WSGI app or Dialect.")
         
-        app = cls(root, **config)
+        if isinstance(root, type):
+            root = root()
         
-        # Automatically use Buffet templating engines unless explicitly forbidden.
-        if asbool(config.get('web.buffet', True)):
-            log.debug("Loading Buffet template engine middleware.")
-            from web.core.middleware import TemplatingMiddleware
-            app = TemplatingMiddleware(app, config)
+        config['web.root'] = root
         
-        # Automatically use ToscaWidgets unless explicitly forbidden (or simply not found).
-        if asbool(config.get('web.widgets', True)):
-            try:
-                from tw.api import make_middleware as ToscaWidgetsMiddleware
-                
-                log.debug("Loading ToscaWidgets middleware.")
-                app = ToscaWidgetsMiddleware(app, config)
-            
-            except ImportError: # pragma: no cover
-                log.warn("ToscaWidgets not installed, widget framework disabled.  You can remove this warning by explicitly defining widgets=False in your config.")
+        app = cls(root, config)
         
-        # Determine if a database engine has been requested, and load the appropriate middleware.
-        if config.get('db.connections', None):
-            try:
-                for connection in aslist(config.get('db.connections')):
-                    connection = connection.strip(',')
-                    
-                    engine = config.get('db.%s.engine' % (connection, ), 'sqlalchemy')
-                    
-                    try:
-                        # FIXME: This shouldn't be restricted to core db engine middleware.
-                        engine = pkg_resources.load_entry_point('YAPWF', 'yapwf.db.engines', engine)
-                    
-                    except:
-                        log.exception("Unable to load engine middleware: %r.", engine)
-                        raise
-                    
-                    try:
-                        model = config.get('db.%s.model' % (connection, ))
-                        model = get_dotted_object(model) if isinstance(model, basestring) else model
-                    
-                    except:
-                        log.exception("Unable to load application model: %r.", model)
-                        raise
-                    
-                    try:
-                        session = config.get('db.%s.session' % (connection, ), '%s:session' % (config.get('db.%s.model' % (connection, )), ))
-                        session = get_dotted_object(session) if isinstance(session, basestring) else session
-                    
-                    except:
-                        log.exception("Unable to load application model session: %r.", session)
-                        raise
-                    
-                    app = engine(app, 'db.%s' % (connection, ), model, session, **config)
-            
-            except:
-                log.exception("Database connections not available.")
-        
-        
-        from paste.config import ConfigMiddleware
-        app = ConfigMiddleware(app, config)
-        
-        # Automatically use beaker-supplied services unless explicitly forbidden (or simply not found).
-        if asbool(config.get('web.beaker', True)):
-            try:
-                from beaker.middleware import SessionMiddleware, CacheMiddleware
-                
-                beakerconfig = dict([(i[7:], j) for i, j in config.iteritems() if i.startswith('beaker.')])
-                
-                log.debug("Loading Beaker session and cache middleware.")
-                app = SessionMiddleware(app, beakerconfig)
-                app = CacheMiddleware(app, beakerconfig)
-                
-                del beakerconfig
-            
-            except ImportError: # pragma: no cover
-                log.warn("Beaker not installed, sessions and caching disabled.  You can remove this warning by specifying beaker=False in your config.")
-        
-        try:
-            if asbool(config.get('debug', False)):
-                log.debug("Debugging enabled; exceptions raised will display an interactive traceback.")
-                
-                from weberror.evalexception import EvalException
-                app = EvalException(app, gconfig)
-            
-            else:
-                log.debug("Debugging disabled; exceptions raised will display a 500 error.")
-                
-                from weberror.errormiddleware import ErrorMiddleware
-                app = ErrorMiddleware(app, gconfig)
-        
-        except ImportError: # pragma: no cover
-            log.warn("WebError not installed, interactive exception handling and messaging disabled.")
-        
-        
-        from paste.registry import RegistryManager
-        app = RegistryManager(app)
-        
-        
-        if asbool(config.get('web.profile', False)):
-            log.debug("Enabling profiling support.")
-            from repoze.profile.profiler import AccumulatingProfileMiddleware
-            
-            app = AccumulatingProfileMiddleware(
-                   app,
-                   log_filename='profile.prof',
-                   discard_first_request=True,
-                   flush_at_shutdown=True,
-                   path='/__profile__'
-               )
-        
-        # Enabled explicitly or while debugging so you can use Paste's HTTP server.
-        if asbool(config.get('web.static', False)) or (config.get('web.static', None) is None and asbool(config.get('debug', False))):
-            from paste.cascade import Cascade
-            from paste.fileapp import DirectoryApp
-            
-            path = config.get('static.path', None)
-            
-            if path is None:
-                # Attempt to discover the path automatically.
-                path = __import__(root.__module__).__file__
-                path = os.path.abspath(path)
-                path = os.path.dirname(path)
-                path = os.path.join(path, 'public')
-            
-            if not os.path.isdir(path):
-                log.warn("Unable to find folder to serve static content from.  Please specify static.path in your config.")
-            
-            log.debug("Serving static files from '%s'.", path)
-            
-            app = Cascade([DirectoryApp(path), app], catch=[401, 403, 404])
-        
-        # Enable compression if requested.
-        if asbool(config.get('web.compress', False)):
-            log.debug("Enabling HTTP compression.")
-            
-            from paste.gzipper import middleware as GzipMiddleware
-            app = GzipMiddleware(app, compress_level=asint(config.get('web.compress.level', 6)))
+        for mware in cls.middleware():
+            app = mware(app, config)
         
         log.info("YAPWF WSGI middleware stack ready.")
         
@@ -222,18 +132,18 @@ class Application(object):
                 start_response('200 OK', [('Content-type', 'text/plain')])
                 return ['%s' % paste.registry.restorer.get_request_id(environment)]
             
-            content = self.root(web.core.request._current_obj())
+            if isinstance(self.root, Dialect):
+                content = self.root(web.core.request._current_obj())
+            
+            else:
+                content = self.root(environment, start_response)
         
         except web.core.http.HTTPException, e:
             return e(environment, start_response)
         
-        # TODO: Handle file-like objects, iterators, and generators.
-        # if is_generator(content): ...
-        
-        # if isinstance(content, types.GeneratorType):
-        #     for i in content:
-        #         yield i
-        #     return
+        if isinstance(content, list) or isinstance(content, types.GeneratorType):
+            web.core.response.app_iter = content
+            return web.core.response(environment, start_response)
         
         if not isinstance(content, basestring):
             return content
--- web/core/middleware.py
+++ web/core/middleware.py
@@ -4,18 +4,303 @@
 """
 
 
+import os
 import pkg_resources
+
+from paste.deploy.converters import asbool, asint, aslist
 
 import web
 from web.utils.object import DottedFileNameFinder
-from web.utils.dictionary import adict
 
 
-__all__ = ['template', 'TemplatingMiddleware']
+__all__ = ['registry', 'middleware', 'template', 'TemplatingMiddleware']
 log = __import__('logging').getLogger(__name__)
 
 _engines = dict((_engine.name, _engine) for _engine in pkg_resources.iter_entry_points('python.templating.engines'))
-_lookup = DottedFileNameFinder()
+_lookup = web.utils.object.DottedFileNameFinder()
+
+
+registry = []
+
+
+
+def defaultbool(value, extended=[]):
+    if value is True: return True
+    
+    if isinstance(value, basestring) and str(value).lower() in (['true', 'on', 'yes'] + extended):
+        return True
+    
+    return False
+
+
+class MiddlewareWrapper(object):
+    def __init__(self, func, name, after=None):
+        self.func, self.name, self.after = func, name, after
+    
+    def __repr__(self):
+        return "Wrapper(%s, %r) for %r" % (self.name, self.after, self.func)
+    
+    def __call__(self, *args, **kw):
+        return self.func(*args, **kw)
+
+
+class middleware(object):
+    def __init__(self, name, after=None):
+        self.name = name
+        self.after = after
+    
+    def __call__(self, func):
+        registry.append(
+                MiddlewareWrapper(
+                        func,
+                        self.name,
+                        self.after
+                    )
+            )
+        
+        return func
+
+
+@middleware('templating')
+def buffet(app, config):
+    # Automatically use Buffet templating engines unless explicitly forbidden.
+    if not defaultbool(config.get('web.templating', True), ['templateinterface']):
+        return app
+    
+    log.debug("Loading Buffet template engine middleware.")
+    return TemplatingMiddleware(app, config)
+
+
+@middleware('widgets', after="templating")
+def toscawidgets(app, config):
+    # Automatically use ToscaWidgets unless explicitly forbidden (or simply not found).
+    if not defaultbool(config.get('web.widgets', True), ['toscawidgets']):
+        return app
+    
+    try:
+        from tw.api import make_middleware as ToscaWidgetsMiddleware
+        
+        log.debug("Loading ToscaWidgets middleware.")
+        return ToscaWidgetsMiddleware(app, config)
+    
+    except ImportError: # pragma: no cover
+        log.warn("ToscaWidgets not installed, widget framework disabled.  You can remove this warning by explicitly defining widgets=False in your config.")
+    
+    return app
+
+
+@middleware('authentication', after="widgets")
+def authkit(app, config):
+    # Use AuthKit if requested.
+    if not defaultbool(config.get('web.auth', False), ['authkit']):
+        return app
+    
+    try:
+        import authkit.authenticate
+        
+        log.debug("Loading AuthKit middleware.")
+        return authkit.authenticate.middleware(app, config)
+    
+    except ImportError: # pragma: no cover
+        log.warn("AuthKit not installed, authentication and authorization disabled.  Your authorization checks, if any, will fail.")
+    
+    return app
+
+
+@middleware('database', after="widgets")
+def database(app, config):
+    # Determine if a database engine has been requested, and load the appropriate middleware.
+    if not config.get('db.connections', None):
+        return app
+    
+    try:
+        for connection in aslist(config.get('db.connections')):
+            connection = connection.strip(',')
+            
+            engine = config.get('db.%s.engine' % (connection, ), 'sqlalchemy')
+            
+            try:
+                if '.' in engine and ':' in engine:
+                    engine = web.utils.object.get_dotted_object(engine)
+                
+                else:
+                    engine = pkg_resources.load_entry_point('YAPWF', 'yapwf.db.engines', engine)
+            
+            except:
+                log.exception("Unable to load engine middleware: %r.", engine)
+                raise
+            
+            try:
+                model = config.get('db.%s.model' % (connection, ))
+                model = web.utils.object.get_dotted_object(model) if isinstance(model, basestring) else model
+            
+            except:
+                log.exception("Unable to load application model: %r.", model)
+                raise
+            
+            try:
+                session = config.get('db.%s.session' % (connection, ), '%s:session' % (config.get('db.%s.model' % (connection, )), ))
+                session = web.utils.object.get_dotted_object(session) if isinstance(session, basestring) else session
+            
+            except:
+                log.exception("Unable to load application model session: %r.", session)
+                raise
+            
+            return engine(app, 'db.%s' % (connection, ), model, session, **config)
+    
+    except:
+        log.exception("Database connections not available.")
+        raise
+    
+    return app
+
+
+@middleware('configuration', after="widgets")
+def configuration(app, config):
+    if not defaultbool(config.get('web.config', True), ['paste']):
+        return app
+    
+    from paste.config import ConfigMiddleware
+    return ConfigMiddleware(app, config)
+
+
+@middleware('sessions', after="widgets")
+def sessions(app, config):
+    if not defaultbool(config.get('web.sessions', True), ['beaker']):
+        return app
+    
+    # Automatically use beaker-supplied services unless explicitly forbidden (or simply not found).
+    try:
+        from beaker.middleware import SessionMiddleware, CacheMiddleware
+        
+        beakerconfig = dict([(i[8:], j) for i, j in config.iteritems() if i.startswith('session.')])
+        
+        log.debug("Loading Beaker session and cache middleware.")
+        bapp = SessionMiddleware(app, beakerconfig)
+        return CacheMiddleware(bapp, beakerconfig)
+    
+    except ImportError: # pragma: no cover
+        log.warn("Beaker not installed, sessions and caching disabled.  You can remove this warning by specifying web.session=False in your config.")
+    
+    return app
+
+
+@middleware('debugging', after=["sessions", "configuration"])
+def debugging(app, config):
+    if not defaultbool(config.get('web.debug', True), ['weberror']):
+        return app
+    
+    try:
+        if asbool(config.get('debug', False)):
+            log.debug("Debugging enabled; exceptions raised will display an interactive traceback.")
+            
+            from weberror.evalexception import EvalException
+            return EvalException(app, config)
+        
+        else:
+            log.debug("Debugging disabled; exceptions raised will display a 500 error.")
+            
+            from weberror.errormiddleware import ErrorMiddleware
+            return ErrorMiddleware(app, config)
+    
+    except ImportError: # pragma: no cover
+        log.warn("WebError not installed, interactive exception handling and messaging disabled.  You can remove this warning by specifying web.debugging=False in your config.")
+    
+    return app
+
+
+@middleware('registry', after="debugging")
+def threadlocal(app, config):
+    from paste.registry import RegistryManager
+    return RegistryManager(app)
+
+
+@middleware('static', after="registry")
+def static(app, config):
+    # Enabled explicitly or while debugging so you can use Paste's HTTP server.
+    if config.get('web.static', None) is None and not asbool(config.get('debug', False)):
+        return app
+    
+    if not asbool(config.get('web.static', False)):
+        return app
+    
+    from paste.cascade import Cascade
+    from paste.fileapp import DirectoryApp
+    
+    path = config.get('static.path', None)
+    
+    if path is None:
+        # Attempt to discover the path automatically.
+        log.info(config['web.root'].__module__)
+        
+        module = __import__(config['web.root'].__module__)
+        parts = config['web.root'].__module__.split('.')[1:]
+        path = module.__file__
+        
+        while parts:
+            # Search up the package tree, in case this is an application in a sub-module.
+            
+            path = os.path.abspath(path)
+            path = os.path.dirname(path)
+            path = os.path.join(path, 'public')
+            
+            log.debug("Trying %r", path)
+            
+            if os.path.isdir(path):
+                break
+            
+            module = getattr(module, parts.pop(0))
+            path = module.__file__
+    
+    if not os.path.isdir(path):
+        log.warn("Unable to find folder to serve static content from.  Please specify static.path in your config.")
+        return app
+    
+    log.debug("Serving static files from '%s'.", path)
+    
+    return Cascade([DirectoryApp(path), app], catch=[401, 403, 404])
+
+
+@middleware('compression', after="static")
+def compression(app, config):
+    if not defaultbool(config.get('web.compress', False), ['paste']):
+        return app
+    
+    # Enable compression if requested.
+    log.debug("Enabling HTTP compression.")
+    
+    from paste.gzipper import middleware as GzipMiddleware
+    return GzipMiddleware(app, compress_level=asint(config.get('web.compress.level', 6)))
+
+
+@middleware('profiling', after="*")
+def profiling(app, config):
+    if not defaultbool(config.get('web.profile', False), ['repoze']):
+        return app
+    
+    log.debug("Enabling profiling support.")
+    
+    try:
+        from repoze.profile.profiler import AccumulatingProfileMiddleware
+    
+        return AccumulatingProfileMiddleware(
+               app,
+               log_filename = config.get('web.profile.log', 'profile.prof'),
+               discard_first_request = asbool(config.get('web.profile.discard', 'true')),
+               flush_at_shutdown = asbool(config.get('web.profile.flush', 'true')),
+               path = config.get('web.profile.path', '/__profile__')
+           )
+    
+    except ImportError: # pragma: no cover
+        log.error("Repoze profiling middleware not installed.")
+    
+    return app
+
+
+
+
+
+
 
 
 
@@ -46,7 +331,7 @@ class TemplatingMiddleware(object):
             return _lookup.get_dotted_filename(template_name, template_extension)
         
         return dict(
-                web=adict(
+                web=web.utils.dictionary.adict(
                         request = web.core.request,
                         response = web.core.response,
                         session = web.core.session
