5ebebe4ea750994cb3c681699264119c4fcfbefd
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-10-12 03:07:17 -0700

    Updated unit tests for TemplateInterface.

--- tests/test_templating.py
+++ tests/test_templating.py
@@ -6,10 +6,11 @@ from webob import Request
 
 import web
 from web.core import Application, Controller
+from cti.middleware import template
 
 
 class RootController(Controller):
-    @web.core.template('templates.test')
+    @template('templates.test')
     def decorator(self):
         return dict()
     
@@ -34,14 +35,14 @@ class TestTemplates(TestCase):
         
         assert response.status == "200 OK"
         assert response.content_type == "text/html"
-        assert response.body == "<html><body><h1>It works!</h1></body></html>"
+        assert response.body == '<html xmlns="http://www.w3.org/1999/xhtml"><body><h1>It works!</h1></body></html>'
     
     def test_tuple(self):
         response = Request.blank('/tuple').get_response(self.app)
         
         assert response.status == "200 OK"
         assert response.content_type == "text/html"
-        assert response.body == "<html><body><h1>It works!</h1></body></html>"
+        assert response.body == '<html xmlns="http://www.w3.org/1999/xhtml"><body><h1>It works!</h1></body></html>'
     
     def test_unicode(self):
         response = Request.blank('/unicode').get_response(self.app)
@@ -49,14 +50,14 @@ class TestTemplates(TestCase):
         assert response.status == "200 OK"
         assert response.content_type == "text/html"
         assert response.charset == 'UTF-8'
-        assert response.unicode_body == u"<html><body><h1>© 2009</h1></body></html>"
+        assert response.unicode_body == u'<html xmlns="http://www.w3.org/1999/xhtml"><body><h1>© 2009</h1></body></html>'
     
     def test_template_globals(self):
         response = Request.blank('/variables').get_response(self.app)
         
         assert response.status == "200 OK"
         assert response.content_type == "text/html"
-        assert response.body == "<html><body><h1>It works!</h1></body></html>"
+        assert response.body == '<html xmlns="http://www.w3.org/1999/xhtml"><body><h1>It works!</h1></body></html>'
     
     def test_exception(self):
         try:
