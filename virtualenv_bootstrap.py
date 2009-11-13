541ded1b9d11912c4819cd7bb63f56d70c7d4a45
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-11-13 01:42:16 -0800

    Updated virtualenv bootstrap generation script.

--- virtualenv_bootstrap.py
+++ virtualenv_bootstrap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import virtualenv, textwrap
+import os, virtualenv, textwrap
 output = virtualenv.create_bootstrap_script(textwrap.dedent("""
 
 import os, subprocess
@@ -35,30 +35,22 @@ def adjust_options(options, args):
     options.use_distribute = True
 
 def after_install(options, home_dir):
-    pip = join(home_dir, 'bin', 'pip')
-    etc = join(home_dir, 'etc')
-    src = join(home_dir, 'src')
-    var = join(home_dir, 'var')
-    tmp = join(home_dir, 'tmp')
+    pip = join(os.getcwd(), home_dir, 'bin', 'pip')
+    etc = join(os.getcwd(), home_dir, 'etc')
+    src = join(os.getcwd(), home_dir, 'src')
+    var = join(os.getcwd(), home_dir, 'var')
+    tmp = join(os.getcwd(), home_dir, 'tmp')
     
     if not os.path.exists(etc): os.makedirs(etc)
     if not os.path.exists(src): os.makedirs(src)
     if not os.path.exists(var): os.makedirs(var)
     if not os.path.exists(tmp): os.makedirs(tmp)
     
-    logger.indent += 2
+    logger.notify('Installing WebCore into virtual environment...')
     
-    try:
-        logger.notify('Installing WebCore into virtual environment...')
-        subprocess.call(
-                [pip, 'install'] + production_packages if options.production else development_packages,
-                cwd = os.path.abspath(home_dir),
-                filter_stdout = filter_python_develop,
-                show_stdout = False
-            )
-    
-    finally:
-        logger.indent -= 2
+    subprocess.call(
+            [pip, 'install'] + (production_packages if options.production else development_packages)
+        )
     
     logger.notify('Run "cd %s ; . bin/activate" to enter the virtual environment.' % (os.path.relpath(home_dir, os.getcwd()), ))
     
@@ -68,3 +60,4 @@ def after_install(options, home_dir):
 
 """))
 f = open('webcore-bootstrap.py', 'w').write(output)
+os.chmod('webcore-bootstrap.py', 0755)
