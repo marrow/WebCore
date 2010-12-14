7c053887199ffac67f028dbe9fd82a2af4fc8f42
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-12-13 23:20:12 -0800

    Version bump.

--- web/release.py
+++ web/release.py
@@ -10,8 +10,8 @@ __all__ = ['name', 'version', 'version_info', 'release', 'summary', 'description
 
 
 name = "WebCore"
-version = "1.0.0"
-version_info = (1, 0, 0, 'production', 0)
+version = "1.0.1"
+version_info = (1, 0, 1, 'production', 0)
 release = "1.0"
 
 summary = "A full-stack, light-weight and efficient web development framework."
@@ -20,6 +20,6 @@ author = "Alice Bevan-McGregor"
 email = "alice@gothcandy.com"
 url = "http://www.web-core.org/"
 download_url = "http://cheeseshop.python.org/pypi/WebCore/"
-copyright = "2009-2010, Alice Bevan-McGregor"
+copyright = "2009-2010, Alice Bevan-McGregor and contributors"
 license = "MIT"
 colophon = """Powered by <a class="noicon" href="http://www.python.org/" title="A programming language that lets you work more quickly and integrate your systems more effectively.">Python</a> <em class="version">""" + "%d.%d" % sys.version_info[:2] + """</em> and <a class="noicon" href="http://www.web-core.org/" title="A lightweight and extremely fast Python web framework.">WebCore</a> <em class="version">""" + release + "</em>."
