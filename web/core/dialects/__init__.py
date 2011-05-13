59fb6f6a97996f9ba5d62d5ac0358b7839ca3a91
Alex Grönholm <alex.gronholm+git@nextday.fi>
2011-05-13 21:50:17 +0300

    Fixed the __all__ import in the web.core.dialects package

--- web/core/dialects/__init__.py
+++ web/core/dialects/__init__.py
@@ -1,8 +1,5 @@
-# encoding: utf-8
-
 from api import Dialect
 from core import Controller
 from rest import RESTMethod
 
-
-__all__ = ['api', 'core', 'rest', 'common', 'Dialect', 'Controller', 'RESTMethod', 'routes']
+__all__ = ('Dialect', 'Controller', 'RESTMethod')
