420ef3f4795da5cca6e04b3117f2aca06fa4ee3d
Alice Bevan-McGregor <alice@gothcandy.com>
2011-12-12 11:36:00 -0500

    Ignore namespace packages in coverage reports.

--- web/db/__init__.py
+++ web/db/__init__.py
@@ -1,4 +1,4 @@
-try:
+try: # pragma: no cover
     __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:
+except ImportError: # pragma: no cover
     __import__('pkgutil').extend_path(__path__, __name__)
