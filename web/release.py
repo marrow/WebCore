eadbeacd4702c923a60b961d495976c39f7c816b
Alice Bevan-McGregor <alice@gothcandy.com>
2011-12-18 19:27:15 -0500

    Version bump.

--- web/release.py
+++ web/release.py
@@ -9,9 +9,9 @@ from collections import namedtuple
 __all__ = ['version_info', 'version', 'copyright', 'colophon']
 
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(1, 1, 0, 'beta', 1)
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(1, 1, 0, 'final', 0)
 
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
 
-copyright = "2009-2010, Alice Bevan-McGregor and contributors"
+copyright = "2009-2011, Alice Bevan-McGregor and contributors"
 colophon = """Powered by <a class="noicon" href="http://www.python.org/" title="A programming language that lets you work more quickly and integrate your systems more effectively.">Python</a> <em class="version">""" + "%d.%d" % sys.version_info[:2] + """</em> and <a class="noicon" href="http://www.web-core.org/" title="A lightweight and extremely fast Python web framework.">WebCore</a> <em class="version">""" + version + "</em>."
