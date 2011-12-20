453fc20254417a3c537c5403babe6d5c3cf329e6
Alex Grönholm <alex.gronholm+git@nextday.fi>
2011-12-20 23:27:47 +0200

    Added almost complete backwards compatibility with the old web.core.i18n module.

--- web/core/i18n.py
+++ web/core/i18n.py
@@ -1,12 +1,26 @@
 # encoding: utf-8
 
 import warnings
-
 import web.core.locale
+
 from web.core.locale import *
+from web.core.middleware import defaultbool, middleware
+from web.core.templating import registry
 
 
 warnings.warn("The 'i18n' module has been renamed 'locale'.\nPlease update your import statements.", DeprecationWarning)
 
 
 __all__ = web.core.locale.__all__
+
+
+@middleware('locale', after="widgets")
+def i18n(app, config):
+    if not defaultbool(config.get('web.i18n', True), ['gettext']):
+        return app
+    
+    from web.core.locale import LocaleMiddleware
+    return LocaleMiddleware(app, config)
+
+# Register the appropriate i18n functions in the global template scope.
+registry.append(dict([('_', _), ('L_', L_), ('N_', N_)]))
