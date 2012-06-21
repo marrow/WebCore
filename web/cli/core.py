dac3a8e18d306c16d94c76ff7c7870f251a26188
Alice Bevan-McGregor <alice@gothcandy.com>
2012-06-21 14:40:02 -0400

    Updated CLI interface to build the sub-commands at import time.

--- web/cli/core.py
+++ web/cli/core.py
@@ -11,67 +11,34 @@ from marrow.script.util import wrap, partitionhelp
 
 from web import release
 
-__all__ = ['ScriptCore']
 
-def discover():
-    """List available templates."""
-        
-    msg = "Available commands:\n"
-        
-    # Load Python plugins.
-    commands = dict((i.name, i.load()) for i in pkg_resources.iter_entry_points('web.command'))
-        
-    # Get the length of the longest plugin name.
-    mlen = max(len(i) for i in commands)
-        
-    # Output one line per command: name, then the first line of documentation (if available).
-    for name in sorted(commands):
-        doc = partitionhelp(getattr(commands[name], '__doc__', 'No description available.'))[0][0]
-        msg += " %-*s  %s" % (mlen, name, wrap(doc).replace("\n", "\n" + " " * (mlen + 3)))
-        
-    msg += "\nIf the last segment of the name is unambiguous you can omit the namespace."
-    return msg   
+__all__ = ['ScriptCore']
 
 
 class ScriptCore(object):
-    """ ScriptCore: command line script for webcore
-    web [--config|-c <config=local.yaml>] [--log|-l <file=STDERR>] [--log-level|-L <level=info>] [cmd] [<cmdopts>]
-    """ + discover()
+    """Extensible command line script dispatcher for the WebCore web framework."""
     
     _cmd_script = dict(
             title = "web",
             version = release.version,
-            copyright = "Copyright 2011 Alice Bevan-McGregor"
+            copyright = "Copyright 2012 Alice Bevan-McGregor and contributors"
         )
     
-    @describe(verbose="Increase logging level to DEBUG.", quiet="Reduce logging level to WARN.")
+    @describe(
+            verbose="Increase logging level to DEBUG.",
+            quiet="Reduce logging level to WARN.",
+            config="The configuration file to use.",
+            log="Where to send logging output; can use STDOUT and STDERR or the specifically named file.",
+            log_level="The default logging level.\nSee marrow.logging documentation for valid values."
+        )
     def __init__(self, verbose=False, quiet=False, config="local.yaml", log="STDERR", log_level="info"):
         pass
     
-    def __getattr__(self, command):
-        # Load Python plugins.
-        available = dict()
-        commands = dict([(i.name, i.load()) for i in pkg_resources.iter_entry_points('web.command')])
-        for name in commands.keys():
-            short = name.partition('.')[2]
-            
-            if short in commands:
-                available[short] = False
-                continue
-            
-            available[short] = commands[name]
-        
-        command = available.get(command, None)
-        
-        if command is None:
-            print("Unknown command.  ", end='')
-            return 1
-        
-        if command is False:
-            print("Ambiguous command.  ", end='')
-            return 2
-        
-        return command
+    # Load plugins.
+    locals().update({
+            i.name: i.load()
+            for i in pkg_resources.iter_entry_points('web.command')
+        })
 
 
 def main():
--- web/cli/versions.py
+++ web/cli/versions.py
@@ -1,17 +1,20 @@
-# -*- encoding: utf-8 -*-
+# encoding: utf-8
 
 import pkg_resources
 
-def versions():
-    """Display versions of installed packages"""
-    seen = dict()
 
-    print("WebCore required packages:")
+def versions(self):
+    """Display versions of installed packages."""
+    seen = dict()
+    
+    print("WebCore and dependencies:\n")
     for i in pkg_resources.require('WebCore'):
         if i in seen: continue
         seen[i] = None
         print(" * {0!r}".format(i))
-
-    print("\nWebCore command line plugins:")
+    
+    print("\nWebCore command line plugins:\n")
     for j in pkg_resources.iter_entry_points('web.command'):
         print(" * {0}: {1!r}".format(j.name, j.dist))
+    
+    print()
