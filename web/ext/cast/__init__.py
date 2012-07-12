b787f46e9b7a7ae7d5f495156f584e422c499767
Alice Bevan-McGregor <alice@gothcandy.com>
2012-07-12 11:10:43 -0400

    Working draft of annotation-based typecasting.

--- web/ext/cast/__init__.py
+++ web/ext/cast/__init__.py
@@ -1,5 +1,12 @@
 # encoding: utf-8
 
+from inspect import getargspec, ismethod
+
+try:
+    from inspect import getfullargspec as getargspec
+except ImportError:
+    pass
+
 
 class CastExtension(object):
     """Typecast the arguments to your controllers using Python 3 function annotations."""
@@ -25,11 +32,25 @@ class CastExtension(object):
 
     def mutate(self, context, handler, args, kw):
         """Inspect and potentially mutate the given handler's arguments.
-        
+
         The args list and kw dictionary may be freely modified, though invalid arguments to the handler will fail.
         """
         annotations = getattr(handler.__func__ if hasattr(handler, '__func__') else handler, '__annotations__', None)
         if not annotations:
             return 
+
+        argspec = getargspec(handler)
+        arglist = list(argspec.args)
         
-        context.log.info("MUTATING")
+        if arglist[0] == 'self':
+            del arglist[0]
+
+        for i, value in enumerate(list(args)):
+            key = arglist[i]
+            if key in annotations:
+                args[i] = annotations[key](value)
+
+        # Convert keyword arguments
+        for key, value in list(kw.items()):
+            if key in annotations:
+                kw[key] = annotations[key](value)
