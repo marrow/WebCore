2f1841d84b292bee467aee1049a6c25bc575af25
Alice Bevan-McGregor <alice@gothcandy.com>
2012-07-14 17:48:00 -0400

    Some cleanup of the template extension; better documentation, new-style __init__.

--- web/ext/template/__init__.py
+++ web/ext/template/__init__.py
@@ -1,34 +1,84 @@
 # encoding: utf-8
 
+# Standard Library Imports
 from weakref import proxy
 from functools import partial
 
+# Third-Party Imports
+from marrow.util.compat import basestring
 from marrow.util.bunch import Bunch
 
+# Local Imports
 from web.core.response import registry
 from web.ext.template.handler import template_handler, annotated_template_handler
-
 from web.ext.template import render
-
 
 
 class TemplateExtension(object):
     """Template rendering extension.
     
-    web.template:
-        engine: mako
-        path: {...}
+    Configuration:
     
+        application:
+            extensions:
+                template:
+                    default: mako
+                    
+                    # Optional configuration directives.
+                    path: /path/to/use/for/relative/templates
+                    override:
+                        somepackage.somemodule: otherpackage.othermodule
+    
+    Setting a default means you won't need to prefix all of your template paths with an engine.  Remember, if you are
+    writing a reusable component, *always* prefix your template paths with an engine!
+    
+    If you define a path here then relative templates (e.g. "mako:./welcome.html") will be relative to that path.
+    Reusable components should never use relative paths.
+    
+    If you define an override mapping (whose values may be a string or list of strings) then templates for the given
+    namespace will search there first.  For example:
+    
+        return 'mako:web.app.blog.templates.comments', dict()
+    
+    With the following override:
+    
+        override:
+            web.app.blog: gothcandy
+    
+    The framework will try look for templates in this order:
+    
+    * gothcandy.templates.comments
+    * web.app.blog.templates.comments
+    
+    Note that these overrides are prefix overrides, you can only replace from the beginning of the template path.
+    
+    Yes, you can also override the template engine, simply prefix your override value with the engine and a colon.
     """
+    
     uses = ['locale']
     provides = ['template']
     
-    def __init__(self, config):
+    def __init__(self, context, default=None, path=None, override=None):
         """Executed to configure the extension."""
         super(TemplateExtension, self).__init__()
         
-        self.path = config.get('path', None)
-        render._render.default = config.get('engine', None)
+        if default and not hasattr(render._render, default):
+            raise ValueError('"{0}" is not a valid engine, perhaps you need to install an adapter?'.format(default))
+        
+        render._render.default = default
+        render.render.path = path
+        
+        if override:
+            override = dict(override)  # we want a copy as a plain dictionary
+            
+            # Ensure that all single values become one-element lists.
+            # Greatly simplifies the per-request code.
+            for key in override:
+                value = override[key]
+                if isinstance(value, basestring):
+                    override[key] = [value]
+        
+        render.render.overrides = override
     
     def start(self):
         """Register the template response handler."""
@@ -38,4 +88,3 @@ class TemplateExtension(object):
     def prepare(self, context):
         context.namespace = Bunch(web=proxy(context))
         context.render = partial(render.render, context)
-        context.render.path = self.path
--- web/ext/template/render.py
+++ web/ext/template/render.py
@@ -10,7 +10,6 @@ from marrow.templating.core import Engines
 _render = Engines()
 _lookup = lambda template: resolve(template)[1]
 resolve = _render.resolve
-registry = []
 
 
 def _relative(parent):
@@ -27,38 +26,28 @@ def render(context, template, variables, **extras):
     
     Keyword arguments, if any, are supplied to the renderer as renderer options.
     
-    Template lookup can be overridden on a per-package basis, configured thusly:
-    
-    web.template.path:
-        web.auth.template: myapp.template.auth
-    
-    You can also directly manipulate the context.render.path dictionary.
-    
-    With the above configuration as an example, a request to render 'web.auth.template.foo' will look in:
-    
-        myapp.template.auth/foo.html
-        web.auth.template/foo.html
-    
+    For a description of available configuraiton options, see the TemplateExtension documentation.
     """
     
     # Do not add any extra data or options to serializers.
     if template[-1] == ':':
         return _render(template, variables, **extras)
     
+    # Prepare the template context.
     data = dict(
             lookup = _lookup,
             relative = _relative(template)
         )
     
-    # TODO: Handle callables.
     data.update(context.namespace)
     data.update(variables)
     
     if 'translator' in context:
         extras.setdefault('i18n', context.translator)
     
-    path = getattr(context.render, 'path', dict())
-    if not path:
+    # Calculate the template overrides.
+    overrides = getattr(render, 'overrides', dict())
+    if not overrides:
         return _render(template, data, **extras)
     
     format = ''
@@ -67,18 +56,23 @@ def render(context, template, variables, **extras):
         format = format + _
     
     # Find all matching overrides, preferring deeper ones.
-    matched = [i for i in path if template.startswith(i)]
-    matched.sort(lambda a, b: len(b) - len(a))
+    matched = [i for i in overrides if template.startswith(i)]
+    matched.sort(key=len, reverse=True)
+    matched = ((source, destination) for source in matched for destination in overrides[source])
     
     result = None
-    for i in matched:
-        tmpl = format + template.replace(i, path[i])
+    for source, destination in matched:
+        tmpl = template.replace(i, overrides[i])
+        
+        if ':' not in tmpl:
+            tmpl = format + tmpl
+        
         try:
             result = _render(tmpl, data, **extras)
         except (ImportError, IOError, ValueError):
             continue
-        else:
-            break
+        
+        break
     
     if not result:
         result = _render(template, data, **extras)
