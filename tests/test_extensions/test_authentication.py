5acc551983a9f100495816f86acea75fb3a5d415
Alex Grönholm <alex.gronholm+git@nextday.fi>
2012-07-27 22:05:04 +0300

    Changed the basic authentication test to use a full Application instead of the extension directly.

--- tests/test_extensions/test_authentication.py
+++ tests/test_extensions/test_authentication.py
@@ -7,10 +7,7 @@ from copy import deepcopy
 
 from nose.tools import eq_
 from marrow.wsgi.objects.request import LocalRequest
-from marrow.wsgi.objects.response import Response
-from marrow import logging
 
-from web.ext.authentication import AuthenticationExtension
 from web.core.application import Application
 
 
@@ -35,12 +32,8 @@ class MockSession(object):
         self.persistent = deepcopy(self.current)
 
 
-class DummyController(object):
-    def __init__(self, context):
-        self._ctx = context
-    
-    def dummymethod(self):
-        pass
+def basic_auth_method(context):
+    return dumps(context.user)
 
 
 def login(context, username, password):
@@ -54,7 +47,14 @@ class TestAuthenticationExtension(object):
     dummy_authenticate = lambda context, username, password: (1, TestAuthenticationExtension.dummy_user) \
             if username == 'foo' and password == 'bar' else (None, None)
     dummy_lookup = lambda context, uid: TestAuthenticationExtension.dummy_user if uid == 1 else None
-    default_basic_config = {'method': 'basic', 'auth_callback': dummy_authenticate, 'lookup_callback': dummy_lookup}
+    default_basic_config = {
+            'extensions': {
+                    'authentication': {
+                            'method': 'basic',
+                            'auth_callback': dummy_authenticate,
+                            }
+                    }
+            }
     default_session_config = {
             'extensions': {
                     'authentication': {
@@ -62,35 +62,25 @@ class TestAuthenticationExtension(object):
                             'lookup_callback': dummy_lookup
                             }
                     }
-    }
-
-    class Context(object):
-        log = logging.log
-    
-    def _init_ext(self, config):
-        self.ext = AuthenticationExtension(self.Context, **config)
-        self.ext.start(self.Context)
+            }
 
     def test_basic_auth(self):
-        self._init_ext(self.default_basic_config)
-        context = self.Context()
-        context.request = LocalRequest({b'HTTP_AUTHORIZATION': b'basic Zm9vOmJhcg=='})
-        context.response = Response(context.request)
-        controller = DummyController(context)
-    
-        self.ext.prepare(context)
-        self.ext.dispatch(context, '', controller, False)
-        self.ext.dispatch(context, 'dummymethod', controller.dummymethod, True)
-    
-        eq_(context.user, self.dummy_user)
+        app = Application(basic_auth_method, self.default_basic_config)
+        request = LocalRequest({b'HTTP_AUTHORIZATION': b'basic Zm9vOmJhcg=='})
+        status, headers, body = app(request.environ)
+
+        user = loads(b''.join(body).decode('utf-8'))
+
+        eq_(status, b'200 OK')
+        eq_(user, self.dummy_user)
 
     def test_session_auth(self):
         app = Application(login, self.default_session_config)
         app.Context.session = MockSession()
-        request = LocalRequest(path='/?username=foo&password=bar')
+        request = LocalRequest(POST={'username': 'foo', 'password': 'bar'})
         status, headers, body = app(request.environ)
 
-        user, session = loads(b''.join(body).decode('utf8'))
+        user, session = loads(b''.join(body).decode('utf-8'))
 
         eq_(status, b'200 OK')
         eq_(user, self.dummy_user)
