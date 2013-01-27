80322610b4892a1d1a03ff85827cdbea5accaf68
Alice Bevan-McGregor <alice@gothcandy.com>
2013-01-27 13:50:19 -0500

    Initial requirements for alternate dispatch selection.

--- web/dialect/dispatch.py
+++ web/dialect/dispatch.py
@@ -3,6 +3,7 @@
 from inspect import isclass, isroutine
 
 from marrow.wsgi.exceptions import HTTPNotFound
+from marrow.util.object import load_object as load
 
 
 def ipeek(d):
@@ -15,42 +16,43 @@ def ipeek(d):
 class ObjectDispatchDialect(object):
     def __init__(self, config):
         self.trailing = config.get('slashes', True)
-    
+        super(ObjectDispatchDialect, self).__init__()
+
     def __call__(self, context, root):
         log = context.log
         path = context.request.remainder
         trailing = False
-        
+
         # Capture and eliminate the final, empty path element.
         # If present there was a trailing slash in the original path.
         if path[-1:] == ['']:
             trailing = True
             path.pop()
-        
+
         # We don't care about leading slashes.
         if path[:1] == ['/']:
             path.consume()
-        
+
         if isclass(root):
             root = root(context)
-        
+
         last = ''
         parent = None
         current = root
-        
+
         # Iterate through and consume the path element (chunk) list.
         for chunk in ipeek(path):
             log.debug(chunk=chunk, chunks=path)
             parent = current
-            
+
             # Security: prevent access to real private attributes.
             # This is tricky as we need to avoid __getattr__ behaviour.
             if chunk[0] == '_' and (hasattr(current.__class__, chunk) or chunk in current.__dict__):
                 raise HTTPNotFound()
-            
+
             current = getattr(parent, str(chunk), None)
             if current: log.data(attr=current).debug("Found attribute.")
-            
+
             # If there is no attribute (real or via __getattr__) try the __lookup__ method to re-route.
             if not current:
                 if not callable(parent):
@@ -68,59 +70,59 @@ class ObjectDispatchDialect(object):
                     else:
                         yield last.split('/'), parent.__call__, True
                     return
-            
+
             if isclass(current):
                 current = current(context)
-            
+
             yield last.split('/'), parent, False
-            
+
             last = str(chunk)
-        
+
         if isclass(current):
             try:
                 current = current(context).__call__
             except AttributeError:
                 raise HTTPNotFound()
-        
+
         yield last.split('/'), current, True
 
 
 def main():
     from marrow.util.bunch import Bunch
     from marrow.logging import Log, DEBUG
-    
+
     request = Bunch(remainder='/')
     context = Bunch(log=Log(level=DEBUG).name(__file__), request=request)
-    
+
     class Controller(object):
         def __init__(self, context):
             self._ctx = context
-    
+
     class RootController(Controller):
         def __call__(self):
             return "Hello world!"
-    
+
     router = ObjectDispatchDialect(dict())
-    
+
     for i,j,k in router(context, RootController):
         context.log.info(path=i,obj=j,final=k)
-    
+
     request.remainder = '/about/staff'
-    
+
     class AboutController(Controller):
         def staff(self):
             return "Staff page."
-    
+
     class RootController(Controller):
         about = AboutController
-    
+
     print()
     for i,j,k in router(context, RootController):
         context.log.info(path=i,obj=j,final=k)
-    
+
     def index(context):
         return "Foo!"
-    
+
     print()
     for i,j,k in router(context, index):
         context.log.info(path=i,obj=j,final=k)
