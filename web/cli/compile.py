023a477d137b1231e163f023518c6a28d5acb387
George Peristerakis <peristerakis@gmail.com>
2013-02-18 21:51:19 -0500

    Fix a bug where if you run web shell it would throw an exception before reaching the python environment

--- web/cli/compile.py
+++ web/cli/compile.py
@@ -9,27 +9,27 @@ import py_compile
 
 def compile(cli, pretend=False):
     """Compile all Python files into bytecode."""
-    
+
     count = 0
-    
+
     for base, directories, files in os.walk('.'):
         if os.path.basename(base).startswith('.'):
             continue
-        
+
         for name in filter(lambda path: os.path.splitext(path)[1] == '.py', files):
             path = os.path.abspath(os.path.join(base, name))
-            
+
             if cli.verbose:
                 print(path)
-            
+
             if not pretend:
                 py_compile.compile(path)
-            
+
             count += 1
-    
+
     if cli.verbose:
         print()
-    
+
     if not pretend:
         print("Compiled {0} source files.".format(count), file=sys.stderr)
     else:
