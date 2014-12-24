1c90fa29d6bb6181017b1a0de9b99edf03b1639d
Alice Bevan–McGregor <alice@gothcandy.com>
2014-12-24 04:44:01 -0800

    Switched to marrow.package extension management, changed context to dict-like.

--- web/core/application.py
+++ web/core/application.py
@@ -4,12 +4,18 @@ from inspect import ismethod #, isclass
 from itertools import chain
 #from weakref import WeakKeyDictionary
 
+try:
+	from collections import UserDict
+except ImportError:  # pragma: no cover
+	from userdict import UserDict
+
 #from marrow.logging import Log, DEBUG
 #from marrow.logging.formats import LineFormat
 from marrow.util.compat import native
 from marrow.util.bunch import Bunch
 from marrow.package.cache import PluginCache
 from marrow.package.loader import load
+from marrow.package.host import ExtensionManager
 from webob.exc import HTTPException
 #from marrow.wsgi.exceptions import HTTPException
 
@@ -29,7 +35,7 @@ class MissingRequirement(ConfigurationException):
 class Application(object):
 	"""The WebCore WSGI application."""
 
-	__slots__ = ('_cache', 'Context', 'log', 'extensions', 'signals', 'dialect_cache')
+	__slots__ = ('_cache', 'Context', 'log', 'extensions', 'signals', 'dialect_cache', 'extension_manager')
 
 	SIGNALS = ('start', 'stop', 'graceful', 'prepare', 'dispatch', 'before', 'after', 'mutate', 'transform')
 
@@ -45,29 +51,48 @@ class Application(object):
 
 		self.log.debug("Preparing extensions.")
 
-		extensions = self.extensions = self.load_extensions(config)
+		self.extension_manager = ExtensionManager('web.extension')
+		extensions = self.extensions = self.extension_manager.order(config, 'extensions')
 		signals = self.signals = self.bind_signals(config, extensions)
 		self.dialect_cache = PluginCache('web.dispatch')
-
+		
 		for ext in signals.start:
 			ext(self.Context)
 
 	def prepare_configuration(self, config):
 		config = Bunch(config) if config else Bunch()
-
+		
 		# We really need this to be there later.
 		if 'extensions' not in config:
 			config.extensions = list()
-
+		
+		config.extensions.insert(0, BaseExtension())
+		
 		return config
 
 	def context_factory(self, root, config):
-		class Context(object):
-			def __contains__(self, name):
-				return hasattr(self, name)
-
+		class Context(dict):
 			def __iter__(self):
-				return ((i, getattr(self, i)) for i in dir(self) if i[0] != '_')
+				return ((i, self[i]) for i in self.keys() if i[0] != '_')
+			
+			def __getattr__(self, name):
+				try:
+					return self[name]
+				except KeyError:
+					pass
+				
+				raise AttributeError("Unknown attribute: " + name)
+			
+			def __setattr__(self, name, value):
+				self[name] = value
+			
+			def __delattr__(self, name):
+				try:
+					del self[name]
+				except KeyError:
+					pass
+				
+				raise AttributeError("Unknown attribute: " + name)
 		
 		Context.app = self
 		Context.root = root
@@ -81,72 +106,7 @@ class Application(object):
 		#		), level=DEBUG).name('base')
 		
 		return Context
-
-	def load_extensions(self, config):
-		extensions = config.extensions  # we iterate this fairly frequently
-
-		# If the base extension isn't present yet, add it.
-		# TODO: Find all who are marked as always=True!
-		if BaseExtension not in extensions:
-			extensions.append(BaseExtension())
-
-		# First let's check if everything needed has been provided.
-		# We deal with 'uses' feature requirements later.
-
-		provided = set().union(*(getattr(ext, 'provides', ()) for ext in extensions))
-		needed = set().union(*(getattr(ext, 'needs', ()) for ext in extensions))
-
-		if not provided.issuperset(needed):
-			raise MissingRequirement("Extensions providing the following features must be configured:\n" +
-					', '.join(needed.difference(provided)))
-
-		# Now we spider the configured extensions and graph. This is a multi-step process.
-
-		# Create a mapping of feature names to extensions. We only want extension objects in our initial graph.
-
-		universal = list()  # these always go first (in any order)
-		inverse = list()  # these always go last (in any order)
-		provides = dict()
-
-		for ext in extensions:
-			for feature in getattr(ext, 'provides', ()):
-				provides[feature] = ext
-
-			if getattr(ext, 'first', False):
-				universal.append(ext)
-			elif getattr(ext, 'last', False):
-				inverse.append(ext)
-
-		# Now we build the initial graph.
-
-		dependencies = dict()
-
-		for ext in extensions:
-			# We build a set of requirements from needs + uses that just happen to have been fulfilled.
-			requirements = set(getattr(ext, 'needs', ()))
-			requirements.update(set(getattr(ext, 'uses', ())).intersection(provided))
-
-			dependencies[ext] = set(provides[req] for req in requirements)
-
-			if universal and ext not in universal:
-				dependencies[ext].update(universal)
-
-			if inverse and ext in inverse:
-				dependencies[ext].update(set(extensions).difference(inverse))
-
-		# Build the final "unidirected acyclic graph"; a list of extensions in dependency-resolved order.
-		dependencies = robust_topological_sort(dependencies)
-
-		# If there are any tuple elements, we've got a circular reference!
-		extensions = []
-		for ext in dependencies:
-			if len(ext) > 1:
-				raise ConfigurationException("Circular dependency found: " + repr(ext))
-
-			extensions.append(ext[0])
-
-		return extensions
-
+	
 	def bind_signals(self, config, extensions):
 		signals = Bunch((signal, []) for signal in self.SIGNALS)
 
@@ -155,13 +115,13 @@ class Application(object):
 				m = getattr(ext, mn, None)
 				if m:
 					signals[mn].append(m)
-
+		
 		signals.after.reverse()
 		signals.mutate.reverse()
 		signals.transform.reverse()
-
+		
 		return signals
-
+	
 	def __call__(self, environ, start_response=None):
 		context = self.Context()
 		context.environ = environ
