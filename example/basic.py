664a03af4fb329e7cbb85962c33d48f09d2ff0dd
Alice Bevan–McGregor <alice@gothcandy.com>
2014-12-25 06:00:41 -0800

    Simplified down.  Now using new serve mechanism.

--- example/basic.py
+++ example/basic.py
@@ -7,15 +7,10 @@ Applications can be as simple or as complex and layered as your needs dictate.
 
 
 def basic(context):
-	#context.log.info("Returning plain text content.")
-	#context.response.mime = b'text/plain'
 	return "Hello world."
 
 
 if __name__ == '__main__':
 	from web.core.application import Application
-	import logging
 	
-	logging.basicConfig(level=logging.WARN)
-	
-	Application(basic).serve('wsgiref')
+	Application(basic).serve('waitress')
