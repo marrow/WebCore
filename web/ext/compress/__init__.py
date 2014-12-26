801e567e1ac4b682b9a7042c01d42eb2b8aebc73
Alice Bevan–McGregor <alice@gothcandy.com>
2014-12-26 05:48:12 -0800

    Removed compression extension anti-feature.

--- web/ext/compress/__init__.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-
-class CompressionExtension(object):
-    provides = ['compression']
-    
-    def __init__(self, context):
-        """Executed to configure the extension."""
-        super(CompressionExtension, self).__init__()
