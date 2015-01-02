4e4c7521de10a23e3169b52b4d394cfd2e7d76fc
Alice Bevan–McGregor <alice@gothcandy.com>
2015-01-02 04:02:10 -0800

    Documentation for the return type registry.

--- web/core/response.py
+++ web/core/response.py
@@ -1,5 +1,30 @@
 # encoding: utf-8
 
+"""Respone / view registry.
+
+WebCore uses a return type (or response) registry to transform values returned by controllers for use as a responese.
+This translation process is used to support the built-in types (see the `base` extension) and can be extended by your
+own application to support additional types.  This is effectively the "view" component of Model-View-Controller.
+
+The `template` extension provides support for template-based views using this mechanism.
+
+## Automatic Transformation of Models
+
+If you frequently hand back serialized data, you may be able to simplify your controller code and reduce boilerplate
+by simply returning your model instances.  By registering a response handler for your model class you can implement
+the serialization in a single, centralized location, making security and testing much easier.
+
+## Exception Processing
+
+When a controller raises an HTTPError subclass it is treated as the return value.  As such you can take specific
+action on any given error, including rendering a pretty error page, etc.  By default these exceptions are treated as
+a WSGI application and are executed, but only if no more specific handlers are registered.
+
+Certain extensions, such as the debugging extension, provide their own exception handlers.
+
+"""
+
+
 log = __import__('logging').getLogger(__name__)
 
 
