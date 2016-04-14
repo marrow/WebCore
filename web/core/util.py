563cff2ffb0d218d1b38ca4ec630b68b24ed278e
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-14 09:33:12 -0400

    Dead code removal and utility cleanup.

--- web/core/util.py
+++ web/core/util.py
@@ -3,7 +3,6 @@
 from __future__ import unicode_literals
 
 from threading import RLock
-from collections import MutableMapping
 
 
 sentinel = object()
@@ -12,10 +11,9 @@ sentinel = object()
 class lazy(object):
 	"""Lazily record the result of evaluating a function and cache the result.
 	
-	This is a non-data descriptor which tells Python to allow the instance __dict__ to override.
+	This is a non-data descriptor which tells Python to allow the instance __dict__ to override. Intended to be used
+	by extensions to add zero-overhead (if un-accessed) values to the context.
 	"""
-	
-	__slots__ = ['__name__', '__module__', '__doc__', 'func']
 	
 	def __init__(self, func, name=None, doc=None):
 		self.__name__ = name or func.__name__
@@ -25,13 +23,14 @@ class lazy(object):
 		self.func = func
 	
 	def __get__(self, instance, type=None):
+		if instance is None:  # Allow direct access to the non-data descriptor via the class.
+			return self
+		
 		with self.lock:  # Try to avoid situations with parallel thread access hammering the function.
-			if instance is None:  # Allow direct access to the non-data descriptor via the class.
-				return self
-			
 			value = instance.__dict__.get(self.__name__, sentinel)
 			
 			if value is sentinel:
-				value = obj.__dict__[self.__name__] = self.func(instance)
-			
-				return value
+				value = instance.__dict__[self.__name__] = self.func(instance)
+		
+		return value
+
