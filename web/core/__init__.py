7dc0601635217bbb137e6cb8d4cc68c4e5dc286f
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-14 09:33:23 -0400

    Populate the web.core namespace.

--- web/core/__init__.py
+++ web/core/__init__.py
@@ -1,8 +1,11 @@
 # encoding: utf-8
 
-# This is to support the web.ext.local extension, and allow for early importing of the variable.
-
 from threading import local as __local
 
+from .application import Application
+from .util import lazy
 
+
+# This is to support the web.ext.local extension, and allow for early importing of the variable.
 local = __local()
+
