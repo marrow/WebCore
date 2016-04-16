5371d2815b5feac7f7d2b236c73fb8ed3cf3e07a
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-16 01:39:57 -0400

    Pin setuptools version for Travis.

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,6 +5,6 @@ set -x
 git config --global user.email "alice+travis@gothcandy.com"
 git config --global user.name "Travis: Marrow"
 
-# pip install --upgrade setuptools pip pytest
+pip install --upgrade 'setuptools<=18.5' pip pytest
 pip install tox tox-travis codecov
 
