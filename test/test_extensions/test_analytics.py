35a5e8717df9a5bcb60593700aa7e2f291816b0f
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-19 11:58:49 -0400

    Add test for full processing pipeline.

--- test/test_extensions/test_analytics.py
+++ test/test_extensions/test_analytics.py
@@ -1,9 +1,20 @@
 # encoding: utf-8
 
 import time
+import pytest
 
+from webob import Request
+from web.core import Application
 from web.core.context import Context
 from web.ext.analytics import AnalyticsExtension
+
+
+def endpoint(context):
+	time.sleep(0.1)
+	return "Hi."
+
+
+sample = Application(endpoint, extensions=[AnalyticsExtension()])
 
 
 def test_analytics_extension():
@@ -19,4 +30,14 @@ def test_analytics_extension():
 	
 	ext.after(ctx)
 	assert 0.1 <= float(ctx.response.headers['X-Generation-Time']) <= 0.2
+
+
+def test_analytics_extension_in_context():
+	try:
+		__import__('web.dispatch.object')
+	except ImportError:
+		pytest.skip("web.dispatch.object not installed")
+	
+	resp = Request.blank('/').get_response(sample)
+	assert 0.1 <= float(resp.headers['X-Generation-Time']) <= 0.2
 
