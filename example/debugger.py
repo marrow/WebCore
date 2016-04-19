c3e32d02333f8873da8f5481794ad4748eba48cc
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-18 20:36:05 -0400

    Example of how to utilize the debugger.

---
+++ example/debugger.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+"""A one-function WebCore 2 demonstration application.
+
+Applications can be as simple or as complex and layered as your needs dictate.
+"""
+
+
+def basic(context, name="world"):
+	return "Hello {name}.".format(name=name)
+
+
+if __name__ == '__main__':
+	from web.core import Application
+	from web.ext.debug import DebugExtension
+	
+	Application(basic, extensions=[DebugExtension()]).serve('waitress')
+
