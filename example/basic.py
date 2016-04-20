6e6685b9e972356df90ad03b6b8b4a7ec6ab4aa5
Alice Bevan–McGregor <alice@gothcandy.com>
2016-04-19 23:17:01 -0400

    Additional documentation.

--- example/basic.py
+++ example/basic.py
@@ -7,6 +7,14 @@ Applications can be as simple or as complex and layered as your needs dictate.
 
 
 def basic(context, name="world"):
+	"""Say hello.
+	
+	This can be tested easily using cURL from the command line:
+	
+		curl http://localhost:8080/  # Default value via GET.
+		curl http://localhost:8080/Alice  # Positionally specified via GET.
+		curl -d name=Eve http://localhost:8080/  # Form-encoded value via POST.
+	"""
 	return "Hello {name}.".format(name=name)
 
 
