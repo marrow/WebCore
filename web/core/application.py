a680bed15c681b0c65ff62c63209625316bc1f5f
Alice Bevan-McGregor <alice@gothcandy.com>
2016-09-20 05:12:20 -0400

    Some substantial changes to argument processing.
    Argument processing is now modular and configurable. Different aspects
    of populating the args and kwargs passed to the endpoints are now split
    among several extension classes which cooperate in mutating the
    arguments.
    
    This netted a 75 r/sec (5%) improvement in performance, slimmed down the
    Application class, as well as benefitting from better handling of rich
    data stored in flat namespaces.

--- web/core/application.py
+++ web/core/application.py
@@ -6,12 +6,11 @@
 
 from __future__ import unicode_literals
 
-from time import time
 import logging
 import logging.config
 
-from inspect import isroutine, isfunction, ismethod, getcallargs
-from webob.exc import HTTPException, HTTPNotFound
+from inspect import isfunction
+from webob.exc import HTTPException, HTTPNotFound, HTTPInternalServerError
 from marrow.package.loader import load
 
 from .context import Context
@@ -19,6 +18,7 @@ from .dispatch import WebDispatchers
 from .extension import WebExtensions
 from .view import WebViews
 from ..ext.base import BaseExtension
+from ..ext import args as arguments
 
 if __debug__:
 	from .util import safe_name
@@ -109,8 +109,20 @@ class Application(object):
 		# We really need this to be there.
 		if 'extensions' not in config: config['extensions'] = list()
 		
-		# Always make sure the BaseExtension is present since request/response objects are handy.
-		config['extensions'].insert(0, BaseExtension())
+		if not any(isinstance(ext, BaseExtension) for ext in config['extensions']):
+			# Always make sure the BaseExtension is present since request/response objects are handy.
+			config['extensions'].insert(0, BaseExtension())
+		
+		if not any(isinstance(ext, arguments.ArgumentExtension) for ext in config['extensions']):
+			# Prepare a default set of argument mutators.
+			config['extensions'].extend([
+					arguments.ValidateArgumentsExtension(),
+					arguments.ContextArgsExtension(),
+					arguments.RemainderArgsExtension(),
+					arguments.QueryStringArgsExtension(),
+					arguments.FormEncodedKwargsExtension(),
+					arguments.JSONKwargsExtension(),
+				])
 		
 		# Tests are skipped on these as we have no particular need to test Python's own logging mechanism.
 		level = config.get('logging', {}).get('level', None)
@@ -144,72 +156,6 @@ class Application(object):
 		# Notify extensions that the service has returned and we are exiting.
 		for ext in self.__context.extension.signal.stop: ext(self.__context)
 	
-	def _extract_arguments(self, request):
-		"""Extract usable args and kwargs from the given request object.
-		
-		Candidate for promotion to an extension callback. Two approaches come to mind:
-		
-		* Simple (fast) last-defined-wins.
-		* Slower "list if defined multiple times".
-			* Optionally merging GET and POST instead of POST overriding.
-			* Optionally PHP-style with [] to denote array elements explicitly.
-		
-		We currently go for the latter without merging or PHP-ness. Additionally, we can parse JSON bodies.
-		
-		TODO: Investigate WebOb's request.urlvars and request.urlargs:
-			https://github.com/Pylons/webob/blob/master/webob/request.py#L566-L646
-		"""
-		
-		args = list(request.remainder)
-		if args and args[0] == '': del args[0]  # Trim the result of a leading `/`.
-		
-		def process_kwargs(src):
-			kwargs = dict()
-			
-			for name_, value in src.items():
-				if name_ in kwargs and not isinstance(kwargs[name_], list):
-					kwargs[name_] = [kwargs[name_], value]
-				elif name_ in kwargs:
-					kwargs[name_].append(value)
-				else:
-					kwargs[name_] = value
-			
-			return kwargs
-		
-		kwargs = process_kwargs(request.GET)
-		kwargs.update(process_kwargs(request.POST))
-		
-		if request.content_type == 'application/json' and request.body:
-			kwargs.update(request.json)
-		
-		return args, kwargs
-	
-	if __debug__:  # This method only exists in development. Really; thus the use of name mangling.
-		def __validate_arguments(self, context, endpoint, bound, args, kwargs):
-			try:
-				if callable(endpoint) and not isroutine(endpoint):
-					# Callable Instance
-					getcallargs(endpoint.__call__, *args, **kwargs)
-				elif bound:
-					# Instance Method
-					getcallargs(endpoint, *args, **kwargs)
-				else:
-					# Unbound Method or Function
-					getcallargs(endpoint, context, *args, **kwargs)
-			
-			except TypeError as e:
-				# If the argument specification doesn't match, the handler can't process this request.
-				# This is one policy. Another possibility is more computationally expensive and would pass only
-				# valid arguments, silently dropping invalid ones. This can be implemented as a mutate handler.
-				log.error(str(e), extra=dict(
-						request = id(context),
-						endpoint = safe_name(endpoint),
-						endpoint_args = args,
-						endpoint_kw = kwargs,
-					))
-				
-				return HTTPNotFound("Incorrect endpoint arguments: " + str(e))
-	
 	def _execute_endpoint(self, context, endpoint, signals):
 		if not callable(endpoint):
 			# Endpoints don't have to be functions.
@@ -224,41 +170,31 @@ class Application(object):
 			# Use the result directly, as if it were the result of calling a function or method.
 			return endpoint
 		
-		# Our endpoint API states that endpoints recieve as positional parameters all remaining path elements, and
-		# as keyword arguments a combination of GET and POST variables with POST taking precedence.
-		args, kwargs = self._extract_arguments(context.request)
-		
-		# Instance methods were handed the context at class construction time via dispatch.
-		# The `not isroutine` bit here catches callable instances, a la "index.html" handling.
-		bound = not isroutine(endpoint) or (ismethod(endpoint) and getattr(endpoint, '__self__', None) is not None)
-	
-		# Allow argument transformation; args and kwargs can be manipulated inline.
-		for ext in signals.mutate: ext(context, endpoint, args, kwargs)
-		
-		if __debug__:
-			log.debug("Callable endpoint located.", extra=dict(
-					request = id(context),
-					endpoint = safe_name(endpoint),
-					endpoint_args = args,
-					endpoint_kw = kwargs
-				))
-			
-			# Make sure the handler can actually accept these arguments when running in development mode.
-			# Passing invalid arguments would 500 Internal Server Error on us due to the TypeError exception bubbling up.
-			result = self.__validate_arguments(context, endpoint, bound, args, kwargs)
-			
-			if result:  # Bail if validation returned any truthy value.
-				return result
+		# Populate any endpoint arguments and allow for chained mutation and validation.
+		args, kwargs = [], {}
 		
 		try:
-			# Actually call the endpoint.
-			if bound:
-				result = endpoint(*args, **kwargs)
-			else:
-				result = endpoint(context, *args, **kwargs)
+			for ext in signals.mutate: ext(context, endpoint, args, kwargs)
 		
 		except HTTPException as e:
 			result = e
+		
+		else:
+			# If successful in accumulating arguments, finally call the endpoint.
+			
+			if __debug__:
+				log.debug("Callable endpoint located and arguments prepared.", extra=dict(
+						request = id(context),
+						endpoint = safe_name(endpoint),
+						endpoint_args = args,
+						endpoint_kw = kwargs
+					))
+			
+			try:
+				result = endpoint(*args, **kwargs)
+			
+			except HTTPException as e:
+				result = e
 		
 		# Execute return value transformation callbacks.
 		for ext in signals.transform: result = ext(context, endpoint, result)
@@ -275,7 +211,6 @@ class Application(object):
 		Most apps won't utilize middleware, the extension interface is preferred for most operations in WebCore.
 		They allow for code injection at various intermediary steps in the processing of a request and response.
 		"""
-		#__import__('pudb').set_trace()
 		context = environ['wc.context'] = self.RequestContext(environ=environ)
 		signals = context.extension.signal
 		
