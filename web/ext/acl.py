e4bfacca4769a64266efdcda8ce4eae6c9896dec
Alice Bevan-McGregor <alice@gothcandy.com>
2016-09-24 01:30:52 -0400

    Added feature, documentation, configuration change.
    * So, that's a lot of documentation, right there.
    * You can now "partially apply" predicate classes easily.
    * Configuration is way, way more robust now.

--- web/ext/acl.py
+++ web/ext/acl.py
@@ -1,20 +1,272 @@
 # encoding: utf-8
 
-"""Access Control List predicate-based security.
+"""Access Control List Security Extension
 
-A predicate, to WebCore, is any callable object that receives the context as its first positional argument and returns
-a boolean, or None. Two methods of making them configurable: functools.partial assignment of keyword arguments, or use
-of classes (configurable via construction) with a __call__ method, accepting the context. Returning True authorizes
-the action the predicate is being used with. False explicitly denies the action the predicate is being used with. None
-means this predicate does not care as to the outcome of authorization, for whatever reason. The default policy if no
-predicates match is to deny, in most instances.
+Predicate-based security for your applications, extensions, and reusable components.
+
+* This extension is **available in**: `WebCore>=2.0.3,<2.1.0`
+* This extension **requires [Python 3](https://www.python.org/)**.
+* This extension has **no external package dependencies**.
+* This extension is **available with the name** `acl` in the `web.ext` namespace.
+* This extension adds the following **context attributes**:
+	* `acl`
+* This extension uses **namespaced plugins** from:
+	* `web.acl.predicate`
+
+1. [**Introduction**](#introduction)
+	2. [Operation](#operation)
+	3. [Predicates](#predicates)
+2. [**Usage**](#usage)
+	1. [Enabling the Extension](#enabling-the-extension)
+		1. [_Imperative Configuration_](#imperative-configuration)
+		2. [_Declarative Configuration_](#declarative-configuration)
+	2. [Defining ACLs](#defining-acls)
+		1. [_Explicit Usage_](#explicit-usage)
+		2. [_Decoration_](#decoration)
+		3. [_Endpoint Return Values_](#endpoint-return-values)
+
+
+# Introduction
+
+This extension provides a method of collecting rules from objects during dispatch descent and subsequently evaluating
+them. This serves the purpose of an access cotrol list (ACL) by allowing these rules to grant access (return `True`),
+explicitly deny access (return `False`), or abstain (return `None`). Additionally, values returned from endpoints will
+have the value of their `__acl__` attribute evaluated, if present.
+
+Also provided are a stock set of _predicates_ which allow for basic boolean logic, various nesting patterns, and
+provide building blocks for more complex behaviour. It is preferred to access these via the `where` helper object,
+whose attributes (also provided as a mapping) are the names of `entry_points` registered plugins.
+
+
+## Operation
+
+On any endpoint or object leading to an endpoint during _dispatch_, define an `__acl__` attribute or property which
+provides an iterable (`set`, `list`, `tuple`, generator, etc.) of _predicate_ objects. Objects may also optionally
+specify an `__acl_inherit__` attribute or property, which, if _falsy_, will clear the ACL that had been built so far
+for the request.
+
+After a final endpoint has been reached, these rules are evaluated in turn (using the `First` predicate), passing the
+request context as their first argument.  Each is called until one either returns `True` to indicate permission has
+been explicitly granted, or returns `False` to indicate permission has been explicitly denied. If no predicates decide
+to have an opinion, the default action is configurable.
+
+
+## Predicates
+
+A _predicate_ is any callable object that optionally accepts a context as its first positional parameter. One might
+look like:
+
+```python
+def always(context=None):
+	return True
+```
+
+That's it, really. The provided built-in ones are class-based, but the process is the same even if the method has a
+strange name like `__call__`.
+
+
+# Usage
+
+## Enabling the Extension
+
+Before utilizing access control list functionality in your own application you must first enable the extension.
+
+Regardless of configuration route chosen rules may be specified as strings, classes, or callables. Strings are
+resolved using the `web.acl.predicate` `entry_point` namespace and further processed. Classes (either directly, or
+loaded by plugin name) are instantiated without arguments and their instance used.
+
+
+### Imperative Configuration
+
+Applications using code to explicitly construct the WebCore `Application` object, but with no particular custom base
+ruleset needed, can pass the extension by name. It will be loaded using its `entry_points` plugin reference.
+
+```python
+app = Application("Hi.", extensions=['acl'])
+```
+
+Applications with a more strict configuration may wish to specify a default rule of `never`. Import the extension
+yourself, and specify a default rule.
+
+```python
+from web.ext.acl import ACLExtension, when
+
+app = Application("Hi.", extensions=[ACLExtension(default=when.never)])
+```
+
+More complex arrangements can be had by specifying rules positionally (their order will be preserved) or by passing a
+`policy` iterable by name. These may be combined with the `default` named argument, with them being combined as
+`positional` + `policy` + `default`.
+
+
+### Declarative Configuration
+
+Using a JSON or YAML-based configuration, you would define your application's `extensions` section either with the
+bare extension declared:
+
+```yaml
+application:
+	root: "Hi."
+	
+	extensions:
+		acl:
+```
+
+Or, specify a default policy by treating the `acl` entry as an array:
+
+```yaml
+application:
+	root: "Hi."
+	
+	extensions:
+		acl:
+			- never
+```
+
+By specifying a singular `default` explicitly:
+
+```yaml
+application:
+	root: "Hi."
+	
+	extensions:
+		acl:
+			default: never
+```
+
+Or, finally, by specifying the `policy`, which must be an array, explicitly:
+
+```yaml
+application:
+	root: "Hi."
+	
+	extensions:
+		acl:
+			policy:
+				- never
+```
+
+Use of `policy` and `default` may be combined, with the default appended to the given policy.
+
+
+## Defining ACLs
+
+Note: We'll be using [object dispatch](https://github.com/marrow/web.dispatch.object/) for these examples.
+
+First, you're going to need to `from web.ext.acl import when` to get easy access to predicates.
+
+
+### Explicit Usage
+
+Define an iterable of predicates using the `__acl__` attribute.
+
+```python
+class PermissiveController:
+	__acl__ = [when.always]
+	
+	def __init__(self, context):
+		pass
+	
+	def __call__(self):
+		return "Hi."
+```
+
+For intermediary nodes in descent and return values, such as a "root controller with method" arrangement, you can
+define an `__acl__` attribute. The contents of this attribute is collected during processing of dispatch events.
+
+
+### Decoration
+
+Using the `when` utility as a decorator or decorator generator.
+
+```python
+@when(when.never)
+class SecureController:
+	def __init__(self, context):
+		pass
+	
+	@when(when.always, inherit=False)
+	def insecure_resource(self):
+		return "Yo."
+	
+	def __call__(self):
+		return "Hi."
+```
+
+You can use the `when` predicate accessor as a decorator, defining the predicates for an object as positional
+parameters.  The result of calling `when` can be saved used later as a decorator by itself, or as a filter to set that
+attribute on other objects.
+
+
+### Endpoint Return Values
+
+Controlling access to information, not just endpoints.
+
+```python
+class Thing:
+	__acl__ = [when.never]
+
+
+def endpoint(context):
+	return Thing()
+```
+
+In this example, `Thing` will not allow itself to be returned by an endpoint. This process is not recursive.
+
+
+# Extending
+
+Defining new predicates is fairly straightforward given the very simple interface. However, because many demands of
+predicates can be resolved entirely by comparison against a value from the request context, two predicate factories
+are provided. These can be used on-demand, or the result can be saved for repeated use later.
+
+
+### Context Value Matches
+
+Grant or deny access based on a value from the context matching one of several possible values.
+
+```python
+local = when.matches(True, 'request.remote_addr', '127.0.0.1', '::1')
+
+@when(local, when.matches(True, 'user.admin', True), when.never)
+def endpoint(context):
+	return "Hi."
+```
+
+This will grant access to local users and those with the `user.admin` flag equal to `True`, as retrieved from the
+context. The `local` predicate has been saved for subsequent use, and demonstrates comparing against any of a number
+of allowable values. The first argument is the intended predicate result if a match is made, the second is the value
+to traverse and compare, and any remaining arguments are treated as acceptable values.
+
+
+### Context Value Contains
+
+Grant or deny access based on a value from the context containing one of several possible values.
+
+```python
+role = when.contains.partial(True, 'user.role')
+
+@when(role('admin', 'editor'))
+def endpoint(context):
+	return "Hi."
+```
+
+This allows you to easily compare against containers such as lists and sets. Also demonstrataed is the ability to
+"partially apply" a predicate, that is, apply some arguments, then apply the rest later.
+
+
 """
 
 # ## Imports
 
 from __future__ import unicode_literals
 
+#from weakref import proxy
+from functools import partial
+from itertools import chain
+from inspect import isclass
 from webob.exc import HTTPForbidden
+from marrow.package.host import PluginManager
 from marrow.package.loader import traverse
 
 from web.core.util import safe_name
@@ -24,6 +276,34 @@ from web.core.util import safe_name
 
 log = __import__('logging').getLogger(__name__)
 
+if __debug__:  # Documentation helpers.
+	__doc_groups__ = {  # Map collapsable sections.
+				'Imperative Configuration': {'config', 'choice'},
+				'Declarative Configuration': {'config', 'choice'},
+				'Explicit Usage': {'sample', 'captioned'},
+				'Decoration': {'sample', 'captioned'},
+				'Endpoint Return Values': {'sample', 'captioned'},
+				'Context Value Matches': {'sample', 'captioned'},
+				'Context Value Contains': {'sample', 'captioned'},
+			}
+
+
+# Plugin manager and decorator.
+
+class _When(PluginManager):
+	"""A derivative of a PluginManager that acts as a decorator.
+	
+	Will assign the `__acl__` property as the tuple of arguments passed.
+	"""
+	def __call__(self, *acl):
+		def acl_when_inner(target):
+			target.__acl__ = acl
+			return target
+		
+		return acl_when_inner
+
+when = _When('web.acl.predicate')  # Easy reference by short name, e.g. when.match(...)
+
 
 # ## Simple Predicates
 
@@ -32,7 +312,11 @@ class Predicate(object):
 	
 	def __call__(self, context=None):
 		raise NotImplementedError()
-
+	
+	@classmethod
+	def partial(cls, *args, **kw):
+		"""Retrieve a partially applied constructor for this predicate."""
+		return partial(cls, *args, **kw)
 
 class Not(Predicate):
 	"""Invert the meaning of another predicate."""
@@ -206,14 +490,16 @@ class ContextMatch(Predicate):
 		return self.grant if result else None
 
 
-class ContextIn(ContextMatch):
+
+
+class ContextContains(ContextMatch):
 	"""Match a variable from the context containing one or more values.
 	
 	Similar to ContextMatch, except matches the values being "in" the target variable rather than comparing equality.
 	
 	Examples might include:
 	
-	reviewer = ContextIn(True, 'user.role', 'reviewer')
+	reviewer = ContextContains(True, 'user.role', 'reviewer')
 	"""
 	
 	__slots__ = ('grant', 'attribute', 'values', 'default')
@@ -242,21 +528,36 @@ class ACLExtension(object):
 	"""
 	
 	provides = {'acl'}
+	extensions = {'web.acl.predicate'}
+	context = {'acl'}
 	
-	def __init__(self, base=None):
+	def __init__(self, *_policy, default=None, policy=None):
 		"""Configure the ACL extension by defining an optional base policy.
 		
 		This policy will be used as the base for every request; these are evaluated first, always.
 		"""
-		self.base_policy = [(None, i, self) for i in base] if base else []
+		
+		if policy is None:
+			policy = []
+		
+		if _policy:
+			policy = chain(_policy, policy)
+		
+		policy = (when[rule] if isinstance(rule, str) else rule for rule in policy)
+		policy = (rule() if isclass(rule) else rule for rule in policy)
+		
+		policy = self.policy = list(policy)
+		
+		if default:
+			policy.append(default)
+		
+		if not policy:  # Grant if the policy is empty.
+			policy.append(when.always)
 	
 	# ### Request-Level Callbacks
 	
 	def prepare(self, context):
-		"""Add the usual suspects to the context.
-		
-		This adds `acl` to the per-request context.
-		"""
+		"""Called to prepare the request context by adding an `acl` attribute."""
 		
 		if __debug__:
 			log.debug("Preparing request context with ACL.", extra=dict(request=id(context)))
