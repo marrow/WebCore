e352af10817c8c758e6e8f60b853b1c0b89b9250
Alice Bevan-McGregor <alice@gothcandy.com>
2019-06-09 23:39:44 -0400

    Updated metadata, bumped minimum Python version.

--- setup.py
+++ setup.py
@@ -1,52 +1,47 @@
-#!/usr/bin/env python
-# encoding: utf-8
+#!/usr/bin/env python3
 
-from __future__ import print_function
-
-import os
-import sys
-import codecs
+from setuptools import setup
+from sys import argv, version_info as python_version
+from pathlib import Path
 
 
-try:
-	from setuptools.core import setup, find_packages
-except ImportError:
-	from setuptools import setup, find_packages
+if python_version < (3, 6):
+	raise SystemExit("Python 3.6 or later is required.")
 
-
-if sys.version_info < (2, 7):
-	raise SystemExit("Python 2.7 or later is required.")
-elif sys.version_info > (3, 0) and sys.version_info < (3, 2):
-	raise SystemExit("CPython 3.3 or Pypy 3 (3.2) or later is required.")
-
-version = description = url = author = author_email = ""  # Silence linter warnings.
-exec(open(os.path.join("web", "core", "release.py")).read())  # Actually populate those values.
-
-here = os.path.abspath(os.path.dirname(__file__))
+here = Path(__file__).resolve().parent
+version = description = url = author = None  # Populated by the next line.
+exec((here / "web" / "core" / "release.py").read_text('utf-8'))
 
 tests_require = [
 		'pytest',  # test collector and extensible runner
 		'pytest-cov',  # coverage reporting
 		'pytest-flakes',  # syntax validation
-		'web.dispatch.object',  # dispatch tests
+		'pytest-isort',  # import ordering
+		'web.dispatch.object>=3.0,<4.0',  # dispatch tests
 		'backlash',  # debug tests
 	]
 
 
-# ## Package Metadata
-
 setup(
-	# ### Basic Metadata
 	name = "WebCore",
 	version = version,
+	
 	description = description,
-	long_description = codecs.open(os.path.join(here, 'README.rst'), 'r', 'utf8').read(),
+	long_description = (here / 'README.rst').read_text('utf-8'),
 	url = url,
 	download_url = 'https://github.com/marrow/WebCore/releases',
+	
 	author = author.name,
 	author_email = author.email,
+	
 	license = 'MIT',
-	keywords = ['marrow', 'web.core', 'web.ext'],
+	keywords = [
+			'marrow',
+			'web.core',
+			'web.ext',
+			'wsgi',
+			'web framework',
+		],
 	classifiers = [
 			"Development Status :: 5 - Production/Stable",
 			"Environment :: Console",
@@ -55,13 +50,10 @@ setup(
 			"License :: OSI Approved :: MIT License",
 			"Operating System :: OS Independent",
 			"Programming Language :: Python",
-			"Programming Language :: Python :: 2",
-			"Programming Language :: Python :: 2.7",
 			"Programming Language :: Python :: 3",
-			"Programming Language :: Python :: 3.2",
-			"Programming Language :: Python :: 3.3",
-			"Programming Language :: Python :: 3.4",
-			"Programming Language :: Python :: 3.5",
+			"Programming Language :: Python :: 3.6",
+			"Programming Language :: Python :: 3.7",
+			"Programming Language :: Python :: 3.8",
 			"Programming Language :: Python :: Implementation :: CPython",
 			"Programming Language :: Python :: Implementation :: PyPy",
 			"Topic :: Internet :: WWW/HTTP :: Dynamic Content",
@@ -70,7 +62,15 @@ setup(
 			"Topic :: Software Development :: Libraries :: Application Frameworks",
 			"Topic :: Software Development :: Libraries :: Python Modules",
 		],
-	# As yet unsupported by setuptools.
+	
+	project_urls = {
+			"Repository": "https://github.com/marrow/WebCore/",
+			"Documentation": "https://github.com/marrow/WebCore/#readme",
+			"Issue Tracker": "https://github.com/marrow/WebCore/issues",
+			"Funding": "https://www.patreon.com/GothAlice",
+		},
+	
+	# Currently unsupported.
 	#document_names = {
 	#		"description": "README.rst",
 	#		"license": "LICENSE.txt",
@@ -78,33 +78,83 @@ setup(
 	#contacts = [
 	#		{"name": "Alice Bevan-McGregor", "email": "alice@gothcandy.com", "role": "author"},
 	#	],
-	#project_urls = {
-	#		"Documentation": "http://pythonhosted.org/WebCore/",
-	#		"Home": "https://github.com/marrow/WebCore/",
-	#		"Repository": "https://github.com/marrow/WebCore/",
-	#		"Tracker": "https://github.com/marrow/WebCore/issues",
-	#	},
 	#environments = [
 	#		"python_version >= '3.2'",
 	#		"'3.0' > python_version >= '2.7'",
 	#	],
 	
-	# ### Code Discovery
+	# Code Discovery
 	
-	packages = find_packages(exclude=['bench', 'docs', 'example', 'test', 'htmlcov']),
+	packages = (  # Define namaespace package contributions.
+			'web.app',  # Application code namaespace.
+			'web.core',  # Framework core.
+			'web.ext',  # Default extension set.
+			'web.server',  # Default WSGI server adapters / bridges.
+		),
 	include_package_data = True,
-	namespace_packages = [
-			'web',  # primary namespace
-			'web.app',  # application code goes here
-			'web.ext',  # framework extensions
-			'web.server',  # front-end WSGI bridges
-		],
-	zip_safe = True,
+	package_data = {'': ['README.rst', 'LICENSE.txt']},
+	zip_safe = False,
 	
-	# ### Plugin Registration
+	python_requires = '~=3.6',
+	
+	setup_requires = [
+			'pytest-runner',
+		] if {'pytest', 'test', 'ptr'}.intersection(argv) else [],
+	
+	install_requires = [
+			'marrow.package~=2.0.0',  # dynamic execution and plugin management
+			'web.dispatch>='
+			'WebOb',  # HTTP request and response objects, and HTTP status code exceptions
+		],
+	
+	tests_require = tests_require,
+	
+	extras_require = {
+			# Recommended Environments
+			'development': tests_require + [  # An extended set of useful development tools.
+					'ptipython',  # Improved Python REPL shell, combination of ptpython and ipython.
+					'pudb',  # Curses-based interactive debugger with Borland stylings.
+					'backlash',  # Web-based interactive REPL shell and traceback explorer.
+					'waitress',  # Recommended development server.
+					'pre-commit',  # Commit validation enforcement.
+					'bandit',  # Automated vulnerability analysis.
+					'e',  # Shell interaction utility.
+				],
+			'production': [  # A default set of production tools.
+					'flup6',  # Python 2 and 3 compatible Flup fork.
+				],
+			
+			# Dispatch Mechanisms
+			'object': ['web.dispatch.object~=3.0'],
+			'route': ['web.dispatch.route~=2.0'],
+			'resource': ['web.dispatch.resource~=3.0'],
+			
+			# General Extras
+			'cli': ['web.command~=3.0'],  # Command-line interface.
+			'template': ['cinje~=1.1.2'],  # Recommended template engine.
+			'database': ['web.db~=3.0', 'pymongo'],  # Recommended database engine.
+			'asset': ['webassets'],  # Recommended static asset management.
+			
+			# Serializers
+			'json': [],  # Nothing extra is required to support JSON; included for completeness.
+			'bson': ['pymongo'],
+			'yaml': ['pyyaml'],
+			'msgpack': ['msgpack'],
+			
+			# Plugin Dependencies
+			'waitress': ['waitress'],
+			'tornado': ['tornado'],
+			'flup': ['flup6'],
+			'cherrypy': ['cherrypy'],
+			'paste': ['paste'],
+			'eventlet': ['eventlet'],
+			'gevent': ['gevent'],
+			'diesel': ['diesel'],
+			'bjoern': ['bjoern'],
+		},
 	
 	entry_points = {
-			# #### Re-usable applications or application components.
+			# Re-usable applications or application components.
 			'web.app': [
 					'static = web.app.static:static',
 				],
@@ -120,14 +170,14 @@ setup(
 					'contains = web.ext.acl:ContextContains',
 				],
 			
-			# #### WebCore Extensions
+			# WebCore Extensions
 			'web.extension': [
-					# ##### BaseExtension, providing request, response, and default views.
+					# BaseExtension, providing request, response, and default views.
 					'base = web.ext.base:BaseExtension',
 					'request = web.ext.base:BaseExtension',
 					'response = web.ext.base:BaseExtension',
 					
-					# ##### Miscelaneous Builtin Extensions
+					# Miscelaneous Builtin Extensions
 					'analytics = web.ext.analytics:AnalyticsExtension',
 					'annotation = web.ext.annotation:AnnotationExtension',  # Preferred use/needs reference.
 					'cast = web.ext.annotation:AnnotationExtension',  # Legacy reference.
@@ -137,7 +187,7 @@ setup(
 					'assets = web.ext.assets:WebAssetsExtension',  # WebAssets integration.
 				],
 			
-			# #### WSGI Server Adapters
+			# WSGI Server Adapters
 			'web.server': [
 					# These are built-in to Python itself, but are reference implementations. Do not rely on these in
 					# production environments; a warning will be issued on attempts to use these if optimizations are
@@ -164,59 +214,11 @@ setup(
 			'web.serialize': [
 					'json = web.ext.serialize:json.dumps',  # JavaScript Object Notation
 					'application/json = web.ext.serialize:json.dumps',  # JavaScript Object Notation
+					'application/json+bson = bson.json_util:dumps[bson]',  # MongoDB Extended JSON
 					'yaml = yaml:dumps[yaml]',  # Yet Another Markup Language
 					'application/x-yaml = yaml:dumps[yaml]',  # Yet Another Markup Language
+					'msgpack = msgpack:packb[msgpack]',  # MessagePack
+					'application/msgpack = msgpack:unpackb[msgpack]',  # MessagePack
 				]
 		},
-	
-	# ## Installation Dependencies
-	
-	setup_requires = [
-			'pytest-runner',
-		] if {'pytest', 'test', 'ptr'}.intersection(sys.argv) else [],
-	install_requires = [
-			'marrow.package<2.0',  # dynamic execution and plugin management
-			'WebOb',  # HTTP request and response objects, and HTTP status code exceptions
-			'pathlib2; python_version < "3.4"',  # Path manipulation utility lib; builtin in 3.4 and 3.5.
-		],
-	tests_require = tests_require,
-	
-	extras_require = {
-			# ### Recommended Environments
-			'development': tests_require + [  # An extended set of useful development tools.
-					'ptpython',  # Improved Python shell.  Run as "ptipython".
-					'ipython',  # Additional extras to improve the Python shell.
-					'pudb',  # Curses-based interactive debugger.
-					'backlash',  # Web-based interactive REPL shell and traceback explorer.
-					'waitress',  # Recommended development server.
-				],
-			'production': [  # A default set of production tools.
-					'flup6',  # Python 2 and 3 compatible Flup fork.
-				],
-			
-			# ### Dispatch Mechanisms
-			'object': ['web.dispatch.object'],
-			'route': ['web.dispatch.route'],
-			'traversal': ['web.dispatch.traversal'],
-			
-			# ### General Extras
-			'cli': ['web.command'],  # Command-line interface.
-			'template': ['web.template', 'cinje'],  # Recommended template engine.
-			'database': ['web.db', 'pymongo'],  # Recommended database engine.
-			'asset': ['webassets'],  # Recommended static asset management.
-			'bson': ['pymongo'],
-			'yaml': ['pyyaml'],
-			
-			# ### Plugin Dependencies
-			'waitress': ['waitress'],
-			'tornado': ['tornado'],
-			'flup': ['flup6'],
-			'cherrypy': ['cherrypy'],
-			'paste': ['paste'],
-			'eventlet': ['eventlet'],
-			'gevent': ['gevent'],
-			'diesel': ['diesel'],
-			'bjoern': ['bjoern'],
-		},
 )
-
