a3a784020dec7b7912a67a3f47165d6128e40e75
Alice Bevan-McGregor <alice@gothcandy.com>
2020-02-10 22:56:33 -0500

    Copyright and version bump.

--- web/core/release.py
+++ web/core/release.py
@@ -4,12 +4,12 @@ import sys
 from collections import namedtuple
 
 
-version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(2, 1, 0, 'beta', 1)
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(3, 0, 0, 'beta', 1)
 version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
 
 author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
 description = "A powerful web development nanoframework so small it's not even a microframework."
-copyright = "2009-2016, Alice Bevan-McGregor and contributors"
+copyright = "2009-2020, Alice Bevan-McGregor and contributors"
 url = 'https://github.com/marrow/WebCore/'
 colophon = """Powered by:
 	<a class="noicon" href="http://www.python.org/" title="A programming language that lets you work more quickly and integrate your systems more effectively.">Python</a> <em class="version">{0.major}.{0.minor}</em>
