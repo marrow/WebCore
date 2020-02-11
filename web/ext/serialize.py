91f7bef2c68e54c906d5ffc76ef6f6d7f0244fc1
Alice Bevan-McGregor <alice@gothcandy.com>
2020-02-10 22:54:47 -0500

    Modernized serialization support.

--- web/ext/serialize.py
+++ web/ext/serialize.py
@@ -1,11 +1,14 @@
 """An example, though quite usable extension to handle list and dictionary return values."""
 
 import pkg_resources
-from collections import Mapping
+from pkg_resources import Distribution, DistributionNotFound
+from collections import Mapping as MappingABC, Iterable as IterableABC
 
+from webob.exc import HTTPNotAcceptable
 from marrow.package.host import PluginManager
-from web.core.compat import str
 
+from ..core.typing import Any, Optional, Iterable, Context
+from .args import ArgumentExtension
 
 try:
 	from bson import json_util as json
@@ -23,56 +26,57 @@ json  # Satisfy linter.
 
 
 class SerializationPlugins(PluginManager):
-	def __init__(self, namespace, folders=None):
+	def __init__(self, namespace:str, folders:Optional[Iterable[str]]=None) -> None:
+		super().__init__(namespace, folders)
+		
 		self.__dict__['names'] = set()
 		self.__dict__['types'] = set()
-		super(SerializationPlugins, self).__init__(namespace, folders)
 	
-	def register(self, name, plugin):
-		super(SerializationPlugins, self).register(name, plugin)
+	def register(self, name:str, plugin:Any) -> None:
+		super().register(name, plugin)
 		
 		self.names.add(name)
-		
-		if '/' in name:
-			self.types.add(name)
+		if '/' in name: self.types.add(name)
 	
-	def _register(self, dist):
-		try:
-			super(SerializationPlugins, self)._register(dist)
-		except pkg_resources.DistributionNotFound:
+	def _register(self, dist:Distribution) -> None:
+		try:  # Squelch the exception by simply not registering the plugin if dependencies are missing.
+			super()._register(dist)
+		except DistributionNotFound:
 			pass
 
 
 # ## Extension
 
-class SerializationExtension(object):
-	"""Sample extension demonstrating integration of automatic serialization, such as JSON.
+class SerializationExtension(ArgumentExtension):
+	"""Sample extension demonstrating integration of automatic bidirectional serialization, such as JSON.
 	
 	This extension registers handlers for lists and dictionaries (technically list and mappings).
 	
 	Additional serializers can be registered during runtime by other extensions by adding a new mimetype mapping
-	to the `context.serialize` dictionary-like object.  For convienence the default serializers are also provided
+	to the `context.serialize` dictionary-like object.  For convenience the default serializers are also provided
 	using their simple names, so you can access the JSON encoder directly, for example:
 	
 		context.serialize.json.dumps(...)
 	"""
 	
-	provides = {'serialization'}
-	extensions = {'web.serializer'}
-	context = {'serialize'}
+	provides: Tags = {'serialization'}
+	extensions: Tags = {'web.serializer'}
+	context: Tags = {'serialize'}
 	
-	def __init__(self, default='application/json', types=(list, Mapping)):
+	def __init__(self, default:str='application/json', types:SerializationTypes=(list, MappingABC)) -> None:
+		super().__init__()
+		
 		self.default = default
 		self.types = types
 	
 	# ### Application-Level Callbacks
 	
-	def start(self, context):
+	def start(self, context:Context) -> None:
 		if __debug__:
 			log.debug("Registering serialization return value handlers.")
 		
 		manager = SerializationPlugins('web.serialize')
-		manager.__dict__['__isabstractmethod__'] = False
+		manager.__dict__['__isabstractmethod__'] = False  # Resolve Python 2.6+ issue.
 		
 		context.serialize = manager
 		
@@ -82,20 +86,25 @@ class SerializationExtension(object):
 	
 	# ### Views
 	
-	def render_serialization(self, context, result):
+	def render_serialization(self, context:Context, result:Any) -> bool:
 		"""Render serialized responses."""
 		
 		resp = context.response
 		serial = context.serialize
 		match = context.request.accept.best_match(serial.types, default_match=self.default)
-		result = serial[match](result)
 		
-		if isinstance(result, str):
-			result = result.decode('utf-8')
+		if match is None:
+			context.response = HTTPNotAcceptable("\n".join(i for i in serial.types if '/' in i))
+			return True
 		
-		resp.charset = 'utf-8'
+		dumps = serial[match]
+		result = dumps(result)
 		resp.content_type = match
-		resp.text = result
+		
+		if isinstance(result, bytes):
+			resp.body = result
+		else:
+			resp.charset = 'utf-8'
+			resp.text = result
 		
 		return True
-
