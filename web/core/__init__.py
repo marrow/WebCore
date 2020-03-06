b0b1af5569f4adb8a4b6fc252100d56464ee7dce
Alice Bevan-McGregor <alice@gothcandy.com>
2020-03-06 17:22:02 -0500

    Cleaned structure, additional documentation.

--- web/core/__init__.py
+++ web/core/__init__.py
@@ -1,8 +1,19 @@
-# Expose these as importable from the top-level `web.core` namespace.
+"""The WebCore web nanoframework.
+
+The primary entry point is a WSGI application implementation creatively named `Application`.
+"""
+
 from .application import Application
 from .util import lazy
 
-__all__ = ['local', 'Application', 'lazy']  # Symbols exported by this package.
+# This is to support the web.ext.local extension, and allow for early importing of the variable. The only "superglobal"
+# supported inherently by WebCore, and only with the presence of that extension: applications and extensions requiring
+# this functionality MUST declare that requirement appropriately, ref: web.ext.local
+local = __import__('threading').local()  # Imported this way to avoid an unnecessary hanging reference to 'threading'.
 
-# This is to support the web.ext.local extension, and allow for early importing of the variable.
-local = __import__('threading').local()
+# Symbols exported by this package.
+__all__ = (
+		'Application',
+		'lazy'
+		'local',
+	)
