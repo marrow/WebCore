f04464d0433500132ba33155b17f5f3fa55ae16a
Alice Bevan-McGregor <alice@gothcandy.com>
2020-06-22 00:22:35 -0400

    Better handling of the case where explicit 'listen' directives are specified.
    Permits multiple binding, reference: `example/basic.py`

--- web/server/waitress_.py
+++ web/server/waitress_.py
@@ -13,9 +13,27 @@ def serve(application:WSGI, host:HostBind='127.0.0.1', port:PortBind=8080, threa
 	"""The recommended development HTTP server.
 	
 	Note that this server performs additional buffering and will not honour chunked encoding breaks.
+	
+	You may specify specific, explicit `listen` directives to be passed to Waitress' constructor. Doing so will
+	prevent automatic addition of `host` and `port` parameters; their values will be silently ignored. Additional
+	keyword parameters will be passed through to `waitress.serve` as-is.
+	
+	This alternate approach allows multiple bindings to be specified, for example, against localhost and the local
+	network adapter's IPv4 address, rather than utilizing a potentially over-eager "all interfaces" host bind, or
+	to simultaneously serve on the same port across IPv4 and IPv6. E.g.:
+	
+		app.serve('waitress', listen='127.0.0.1:8080 [::1]:8080')
+	
+	For a "living" example of this, see `example/basic.py` / `.md` within the WebCore repository. (Useful to silence
+	cURL initial connection attempt failure warnings if your system is built with IPv6 support.)
 	"""
 	
 	assert check_argument_types()
 	
+	if 'listen' not in kw:
+		kw = {'host': host, 'port': port, **kw}
+	
+	kw['threads'] = threads
+	
 	# Bind and start the server; this is a blocking process.
-	serve_(application, host=str(host), port=int(port), threads=int(threads), **kw)
+	serve_(application, **kw)
