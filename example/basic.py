c6b91ca9c95a4beb436410d962a9e3df5760bacb
Alice Bevan-McGregor <alice@gothcandy.com>
2021-02-28 22:23:03 -0500

    Moved import, switched to f-strings, adjusted annotation and improved documentation.

--- example/basic.py
+++ example/basic.py
@@ -3,13 +3,21 @@
 Applications can be as simple or as complex and layered as your needs dictate.
 """
 
-from web.core.typing import Context
+import logging
+
+from web.core.typing import Context, Optional
 
 
-def greet(context:Context, name:str="world") -> str:
+def greet(context:Optional[Context]=None, name:str="world") -> str:
 	"""Say hello.
 	
-	This can be tested easily using cURL from the command line:
+	The context is optional (possible to omit or None may be passed in) because in this example it is unused. This has
+	the side-effect of making this function a simple, usable Python function, usable outside the context of a web
+	interface. However, spinning this up for testing as a web service can be easily accomplished by executing:
+	
+		python example/basic.py
+	
+	Then utilize cURL from the command prompt:
 	
 		curl -v http://localhost:8080/  # Default value via GET.
 		curl -v http://localhost:8080/Alice  # Positionally specified via GET.
@@ -27,19 +35,17 @@ def greet(context:Context, name:str="world") -> str:
 		python3 -X dev example/basic.py
 	"""
 	
-	import logging
 	logging.debug("This is a diagnostic situation.")
 	logging.info("This is an informational situation.", extra=dict(foo="bar", baz=27))
 	logging.warning("This is a warning situation.")
 	logging.error("This is an error situation.")
 	logging.critical("This is a critical situation.")
 	
-	return "Hello {name}.".format(name=name)
+	return f"Hello {name}."
 
 
 if __name__ == '__main__':
 	from web.core import Application
 	
-	Application(greet, extensions=[
-			'kwargs.elision',
-		], logging={'level': 'info'}).serve('waitress', listen='127.0.0.1:8080 [::1]:8080', threads=16)
+	Application(greet, extensions=['kwargs.elision'], logging={'level': 'debug' if __debug__ else 'info'}) \
+			.serve('waitress', listen='127.0.0.1:8080 [::1]:8080', threads=4)
