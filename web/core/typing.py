e1eed88cfca4c0048ef3eb74f148ad99e020fc84
Alice Zoë Bevan–McGregor <alice@gothcandy.com>
2023-09-05 09:53:11 -0400

    Add Python 3 type hinting annotation helpers and status code-based internal redirection extension.

---
+++ web/core/typing.py
@@ -0,0 +1,78 @@
+"""Typing helpers."""
+
+from logging import Logger
+from pathlib import Path, PurePosixPath
+from types import ModuleType
+from typing import Any, Callable, ClassVar, Dict, Generator, Iterable, List, Mapping, Optional, Set, Tuple, Union, \
+		Text, Type, Pattern, MutableSet
+
+from typeguard import check_argument_types
+from uri import URI
+from webob import Request, Response
+
+from ..dispatch.core import Crumb
+from .context import Context  # Make abstract?  :'(
+
+# Core application configuration components.
+
+AccelRedirectSourcePrefix = Union[str, Path]
+AccelRedirectSourceTarget = Union[str, PurePosixPath, URI]
+AccelRedirect = Optional[Tuple[AccelRedirectSourcePrefix, AccelRedirectSourceTarget]]
+
+
+# Types for WebCore extension component parts.
+
+Tags = Set[str]  # Extension feature and dependency tags.
+PositionalArgs = List[Any]  # Positional arguments to the endpoint callable.
+KeywordArgs = Dict[str, Any]  # Keyword arguments to the endpoint callable.
+Environment = Dict[str, Any]  # An interactive shell REPL environment.
+
+
+# Types for WSGI component parts.
+
+# Passed to the WSGI application.
+WSGIEnvironment = Dict[Text, Any]
+
+# Passed to start_response.
+WSGIStatus = str
+WSGIHeaders = List[Tuple[str, str]]
+WSGIException = Optional[Tuple[Any, Any, Any]]
+
+# Returned by start_response.
+WSGIWriter = Callable[[bytes], None]
+
+WSGIResponse = Union[
+		Generator[bytes, None, None],
+		Iterable[bytes]
+	]
+
+# Types for core WSGI protocol components.
+
+# Passed to the WSGI application.
+WSGIStartResponse = Callable[[WSGIStatus, WSGIHeaders, WSGIException], WSGIWriter]
+
+# WSGI application object itself.
+WSGI = Callable[[WSGIEnvironment, WSGIStartResponse], WSGIResponse]
+
+
+# Types relating to specific forms of callback utilized by the framework.
+
+# The `serve` web application/server bridge API interface.
+HostBind = str
+PortBind = int
+DomainBind = Optional[Union[str,Path]]
+WebServer = Callable[..., None]  # [WSGI, HostBind, PortBind, ...]
+
+# Endpoint return value handlers.
+View = Callable[[Context,Any],bool]
+
+# Serialization extension related typing.
+SerializationTypes = Iterable[type]
+Serializer = Callable[[Any], str]
+Deserializer = Callable[[str], Any]
+
+
+# Specific utility forms.
+
+PatternString = Union[str, Pattern]
+PatternStrings = Iterable[PatternString]
---
+++ web/ext/handler.py
@@ -0,0 +1,108 @@
+"""Permit the specification of internal routes to utilize to replace responses having specific statuses.
+
+This is typically used to implement error page responses using internal redirection. The initially returned status
+code is utilized for the ultimate response, so no need to worry that your error page handlers also utilize the error
+status.
+
+Example usage, imperative configuration as WSGI middleware: (ignoring that this is a WebCore extension)
+
+	ext = StatusHandlers()
+	ext[404] = '/fourohfour'
+	ext[HTTPInternalServerError] = '/died'
+	app = ext(None, app)  # This extension does not utilize the first argument, an application context.
+
+Immediate declarative, rather than imperative configuration:
+
+	ext = StatusHandlers({
+			404: '/fourohfour',
+			HTTPInternalServerError: '/died',
+		})
+
+"""
+
+from os import environ, getenv as env
+from urllib.parse import unquote_plus
+from typing import Mapping, Optional, Union
+
+from webob import Request, Response
+from webob.exc import HTTPError
+
+from web.core.typing import Context, \
+		WSGI, WSGIEnvironment, WSGIStartResponse, WSGIStatus, WSGIHeaders, WSGIException, WSGIWriter
+
+
+StatusLike = Union[int, Response, HTTPError]
+
+
+class StatusHandlers:
+	handlers: Mapping[int, str]
+	
+	def __init__(self, handlers:Optional[Mapping[int, str]]=None):
+		self.handlers = handlers or {}
+	
+	def __normalize(self, status:StatusLike) -> int:
+		status = getattr(status, 'status_int', getattr(status, 'code', status))
+		if not isinstance(status, int): raise TypeError("HTTP status code must be an integer, Response-, or HTTPException-compatible type.")
+		return status
+	
+	@property
+	def _maintenance(self) -> bool:
+		"""Identify if the application is running in "maintenance mode".
+		
+		"Maintenance mode" will always show the result of the 503 error handler.
+		"""
+		
+		# Pull the maintenance status from the application environment, by default, excluding local development.
+		return bool(env('MAINTENANCE', False)) and not __debug__
+	
+	# Proxy dictionary-like access through to the underlying status code mapping, normalizing on integer keys.
+	
+	def __getitem__(self, status:StatusLike) -> str:
+		"""Retrieve the path specified for handling a specific status code or HTTPException."""
+		return self.handlers[status]
+	
+	def __setitem__(self, status:StatusLike, handler:str) -> None:
+		"""Assign a new handler path for the given status code or HTTPException."""
+		status = self.__normalize(status)
+		self.handlers[status] = handler
+	
+	def __delitem__(self, status:StatusLike) -> None:
+		"""Remove a handler for the specified status code or HTTPException."""
+		status = self.__normalize(status)
+		del self.handlers[status]
+	
+	# WebCore extension WSGI middleware hook.
+	
+	def __call__(self, context:Context, app:WSGI) -> WSGI:
+		"""Decorate the WebCore application object with middleware to interpose and internally redirect by status."""
+		
+		def middleware(environ:WSGIEnvironment, start_response:WSGIStartResponse) -> WSGIResponse:
+			capture = []
+			
+			if self._maintenance:
+				request = Request.blank(self.handlers[503])
+				result = request.send(app, catch_exc_info=True)
+				start_response(b'503 Service Unavailable', result.headerlist)
+				return result.app_iter
+			
+			def local_start_response(status:WSGIStatus, headers:WSGIHeaders, exc_info:WSGIException=None) -> WSGIWriter:
+				status = status if isinstance(status, str) else status.decode('ascii')
+				status_code = int(status.partition(' ')[0])
+				
+				if status_code not in self.handlers:
+					return start_response(status, headers)
+				
+				capture.extend((status, headers, exc_info, self.handlers.get(status_code, None)))
+			
+			result = app(environ, local_start_response)
+			
+			if not capture: return result
+			
+			request = Request.blank(capture[-1])
+			result = request.send(app, catch_exc_info=True)
+			
+			start_response(capture[0], result.headerlist)
+			return result.app_iter
+		
+		return middleware
+
