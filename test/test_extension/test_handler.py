a67ddd34840e3b409d3cd40192c54630de29291a
Alice Zoë Bevan–McGregor <alice@gothcandy.com>
2023-09-05 13:17:09 -0400

    Initial WIP tests for status code handlers.

---
+++ test/test_extension/test_handler.py
@@ -0,0 +1,33 @@
+import pytest
+from webob import Request
+from webob.exc import HTTPNotFound
+
+from web.core import Application
+from web.core.testing import MockRequest
+from web.core.ext.handler import StatusHandlers
+
+
+def mock_endpoint(context, status, state=None):
+	context.response.status_int = int(status)
+	mock_endpoint.state = state
+	return state
+
+
+@pytest.fixture
+def app():
+	return Application(mock_endpoint, [StatusHandlers({
+			HTTPNotFound: '/404/notfound',
+			503: '/503/maintenance',
+		})])
+
+
+def test_handlers(app):
+	assert getattr(mock_endpoint, 'state', None) is None
+	
+	with MockRequest('/404') as request:
+		response = request.send(app)
+	
+	assert response.text == 'notfound'
+	assert response.status_int == 404
+	assert mock_endpoint.state == 'notfound'
+
